3e55ec17bbe028c488e914721ee4da0d8d7685ec
Бахтигреев Дмитрий Николаевич <185369@edu.fa.ru>
2019-09-24 16:28:08 +0300

    Tasks 1,2,3

--- client.py
+++ client.py
@@ -3,14 +3,16 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+msg = ""
+data = []
+while True:
+	msg = input()
+	if msg == "exit":
+		break
+	sock.send(msg.encode())
+	data.append(sock.recv(1024).decode())
 sock.close()
 
-print(data.decode())
+print(data)
--- server.py
+++ server.py
@@ -2,19 +2,21 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+sock.listen(1)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	conn, addr = sock.accept()
+	print(addr)
 
-print(msg)
+	msg = ''
 
-conn.close()
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg += data.decode()
+		conn.send(data)
+
+	print(msg)
+
+	conn.close()
