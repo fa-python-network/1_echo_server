d3cda78dd4d55ba54e5fed1a19e66e2bc1af0ad5
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-09-26 22:54:32 +0300

    Пункт 5

--- server.py
+++ server.py
@@ -1,18 +1,34 @@
 import socket
+from datetime import datetime
 
-port =  int(input("Порт:"))
-port = port if (port >= 0 and port <= 65535)  else  9090
-while True:
-    sock = socket.socket()
-    sock.bind(('', port))
-    sock.listen(1)
-    conn, addr = sock.accept()
-    data=list()
-    while True:
-        msg = conn.recv(1024)
-        if msg:
-            data.append(msg.decode())
-            print(data[-1])
-        else:
-            conn.close()
-            break
+class Server():
+	def __init__(self, log = "file.log"):
+		self.__log = log
+		self.port =  int(input("Порт:"))
+		self.port = self.port if (self.port >= 0 and self.port <= 65535)  else  9090
+	@property
+	def startServer(self):
+		while True:
+			sock = socket.socket()
+			sock.bind(('', self.port))
+			sock.listen(1)
+			conn, addr = sock.accept()
+			self.serverStarted(addr)
+			data=list()
+			while True:
+				msg = conn.recv(1024)
+				if msg:
+					data.append(msg.decode())
+					print(data[-1])
+				else:
+					conn.close()
+					self.serverStopped(addr)
+					break
+	def serverStarted(self,ip):
+		with open(self.__log, "a", encoding="utf-8") as f:
+			print(f"{datetime.now().time()} Server Launched {ip}", file=f)
+	def serverStopped(self,ip):
+		with open(self.__log, "a", encoding="utf-8") as f:
+			print(f"{datetime.now().time()} Server Stopped {ip}", file = f)
+server = Server()
+server.startServer
