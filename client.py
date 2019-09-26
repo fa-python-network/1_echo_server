b58732b65d3a123de1965c8edd05e46044709f88
Дмитрий Гриднев <flymedllva@gmail.com>
2019-09-27 00:31:08 +0300

    Асинхронный сервер

--- client.py
+++ client.py
@@ -1,16 +1,68 @@
-import socket
-from time import sleep
+from socket import socket
+from threading import Thread
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+class Client(socket):
+    BUFFER_SIZE = 1024
+    ENCODING = 'utf-8'
 
-data = sock.recv(1024)
+    def __init__(self, host: str = "localhost", port: int = 9090):
+        """
+        Конструктор клиента
 
-sock.close()
+        :param host:
+        :param port:
+        """
 
-print(data.decode())
+        super(Client, self).__init__()
+        self.connect((host, port))
+        self.listener_thread = Thread(target=self.listener)
+        self.listener_thread.start()
+        self.main_loop()
+
+    def main_loop(self):
+        try:
+            while True:
+                message = input('| ')
+                self.send(message.encode(self.ENCODING))
+        except EOFError:
+            pass
+        finally:
+            self.close()
+
+    def listener(self):
+        try:
+            no_data = 0
+            while True and no_data <= 3:
+                data = self.recv(self.BUFFER_SIZE).decode(self.ENCODING)
+                if data == "":
+                    no_data += 1
+                else:
+                    no_data = 0
+                print(' *server* => ', data)
+        except ConnectionAbortedError:
+            pass
+
+
+if __name__ == '__main__':
+
+    # Тоже не удобно  :)
+
+    # answer_host = input("Хотите ввести адрес хоста?  ")
+    # if answer_host.lower() == "y":
+    #     answer_host = input("Введите адрес хоста")
+    # else:
+    #     answer_host = "localhost"
+    #
+    # answer_port = input("Хотите ввести адрес хоста?  ")
+    # if answer_port.lower() == "y":
+    #     try:
+    #         answer_port = int(input("Введите адрес хоста"))
+    #     except ValueError:
+    #         answer_port = 9090
+    # else:
+    #     answer_port = 9090
+    #
+    # client = Client(host=answer_host, port=answer_port)
+
+    client = Client(port=9090)
--- server.py
+++ server.py
@@ -1,20 +1,336 @@
-import socket
+from socket import socket, SO_REUSEADDR, SOL_SOCKET
+from asyncio import Task, get_event_loop
+from threading import Thread
+from datetime import datetime
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+class Loger(Thread):
+    """
+    Логер в отдельном потоке
+    """
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    def __init__(self, host: str, port: int, file_name: str = "logs.log"):
+        """
+        Конструктор логера
 
-print(msg)
+        :param file_name:
+        :param host:
+        :param port:
+        """
 
-conn.close()
+        Thread.__init__(self)
+        self.file_name = file_name
+        self.host = host
+        self.port = port
+
+    def write_to_file(self, data: str, file_name: str = None):
+        """
+        Пишет данные в файл
+
+        :param data:
+        :param file_name:
+        :return:
+        """
+
+        if file_name is None:
+            file_name = self.file_name
+
+        with open(file_name, "a", encoding="utf-8") as file_one:
+            print(data, file=file_one)
+
+        return data
+
+    def run(self):
+        """
+        Срабатывает автоматический при запуске логера
+
+        :return:
+        """
+
+        return self.info_log("Логер запущен")
+
+    def info_log(self, text: str):
+        """
+        Лог информации
+
+        :param text:
+        :return:
+        """
+
+        text = f"{self.today()} – INFO – {text}"
+        print(self.write_to_file(text))
+
+    def server_is_running(self):
+        """
+        Запуск сервера
+
+        :return:
+        """
+
+        return self.info_log(f"Сервер {self.host}:{self.port} запущен")
+
+    def server_is_stopped(self):
+        """
+        Остановка сервера
+
+        :return:
+        """
+
+        return self.info_log("Сервер остановлен")
+
+    def start_listening_port(self, port):
+        """
+        Начало прослушивания порта
+
+        :param port:
+        :return:
+        """
+
+        return self.info_log(f"Запущено пропуслушивание порта {port}")
+
+    def new_connection(self, user: tuple):
+        """
+        Новое подключение
+
+        :return:
+        """
+
+        return self.info_log(f"Пользователь {user[0]}:{user[1]} подключен")
+
+    def connection_closed(self, user: tuple):
+        """
+        Закрытие соединения
+
+        :return:
+        """
+
+        return self.info_log(f'Пользователь {user[0]}:{user[1]} отключился')
+
+    def socket_closed_other_side(self, user):
+        """
+        Сокет был закрыт другой стороной
+
+        :return:
+        """
+
+        return self.info_log(f"Пользователь {user[0]}:{user[1]} попросил попросил закрыть его сокет")
+
+    def send_data(self, user: tuple, data: str):
+        """
+        Отправка данных сервером
+
+        :param user:
+        :param data:
+        :return:
+        """
+
+        return self.info_log(f"Сервер отправил сообщение «{data}» пользователю {user[0]}:{user[1]}")
+
+    def obtain_data(self, user: tuple, data: str):
+        """
+        Получение данных
+
+        :param user:
+        :param data:
+        :return:
+        """
+
+        return self.info_log(f"Пользователь {user[0]}:{user[1]} прислал «{data}»")
+
+    @staticmethod
+    def today():
+        """
+        Возвращает текущий день
+
+        :return:
+        """
+
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Peer(object):
+
+    ENCODING = "utf-8"
+    BUFFER_SIZE = 1024
+
+    def __init__(self, sock_server: 'Server', sock: socket, address: tuple, loger: Loger):
+        """
+        Конструктор сокета
+
+        :param sock_server:
+        :param sock:
+        :param address:
+        """
+
+        self.loop = sock_server.loop
+        self.address = address
+        self.loger = loger
+        self._sock = sock
+        self._server = sock_server
+        Task(self._peer_handler())
+
+    async def send(self, data):
+        """
+        Отправляет данные текущему сокету
+
+        :param data:
+        :return:
+        """
+
+        await self.loop.sock_sendall(self._sock, data.encode(self.ENCODING))
+
+    async def _peer_handler(self):
+        """
+        Получает новых данных для сокета экземлпляра текущего класса
+
+        :return:
+        """
+
+        try:
+            await self._peer_loop()
+        except IOError:
+            pass
+        finally:
+            self._server.peer_remove(self)
+
+    async def _peer_loop(self):
+        """
+        Получает сообщение пользователя
+
+        :return:
+        """
+
+        while True:
+            data = await self.loop.sock_recv(self._sock, self.BUFFER_SIZE)
+            if data == b"":
+                break
+            data = data.decode(self.ENCODING)
+            self.loger.obtain_data(self.address, data)
+            if data == "exit":
+                self.loger.socket_closed_other_side(self.address)
+                self._server.peer_remove(self)
+
+
+class Server(object):
+
+    def __init__(self, host: str = "localhost", port: int = 9090, listen: int = 100):
+        """
+        Конструктор сервера
+
+        :param host:
+        :param port:
+        :param listen:
+        """
+
+        self.loop = get_event_loop()
+        self._server_sock = socket()
+        self._server_sock.setblocking(False)
+        self._server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self._server_sock.bind((host, port))
+        self._server_sock.listen(listen)
+        self.loger = Loger(host, port)
+        self.loger.run()
+        self._users = list()
+        Task(self._server())
+        self.loger.server_is_running()
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            self.loger.server_is_stopped()
+
+    def peer_remove(self, peer: Peer):
+        """
+        Удаляет прослушиваемый сокет из сервера
+
+        :param peer:
+        :return:
+        """
+
+        self._users.remove(peer)
+        self.loger.connection_closed(peer.address)
+
+    async def _server(self):
+        """
+        Принимает новые соединения с серверного сокета
+
+        :return:
+        """
+
+        while True:
+            user_sock, client = await self.loop.sock_accept(self._server_sock)
+            user_sock.setblocking(False)
+            user = Peer(self, user_sock, client, self.loger)
+            self._users.append(user)
+            self.loger.new_connection(user.address)
+            message = f"Привет {user.address[0]}:{user.address[1]}!"
+            await user.send(message)
+            self.loger.send_data(user.address, message)
+
+
+class Ports(socket):
+
+    def __init__(self, host: str = "localhost", min_port: int = 9090, max_port: int = 9100):
+        """
+        Конструктор проверки портов
+
+        :param host:
+        :param min_port:
+        :param max_port:
+        """
+        super(Ports, self).__init__()
+        self.host = host
+        self.min_port = min_port
+        self.max_port = max_port
+        self.free_port = self.__free_port()
+
+    def select_port(self):
+        """
+        Выбор свободного порта
+
+        :return:
+        """
+
+        port = self.free_port
+        if port is None:
+            while True:
+                port = input("Не нашли свободный порт в заданном диапазоне\nВведите порт, который требуется запустить "
+                             "насильно: ")
+                if port.isdigit():
+                    port = int(port)
+                    if 1 <= port <= 65535:
+                        return port
+        else:
+            return port
+
+    def __free_port(self):
+        """
+        Ищет свободный порт в заданном диапазоне
+
+        :return:
+        """
+
+        for port in range(self.min_port, self.max_port):
+            try:
+                self.bind((self.host, port))
+                self.close()
+                return port
+            except OSError:
+                pass
+        return None
+
+
+if __name__ == '__main__':
+
+    # Это для будущих поколений(4 задание), я думаю оно только мешает, ибо порт задается насольно, и я уверен,
+    # что он свободен
+
+    # answer = input("Хотите ввести адрес хоста?  ")
+    # if answer.lower() == "y":
+    #     answer = input("Введите адрес хоста")
+    # else:
+    #     answer = "localhost"
+    # Тоже самое про порт :/
+    # server = Server(host=answer, port=Ports().select_port())
+    # Написано за 1 час, 30 минут
+
+    server = Server(port=Ports().select_port())
