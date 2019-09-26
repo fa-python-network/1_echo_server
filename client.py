611d268935bf9500de1095b46863ad89645cb82a
Alexandra Kolomenskaya <avkolomenskaya@gmail.com>
2019-09-26 21:23:35 +0300

    2-5 пункты

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 import socket
-from time import sleep
+
+try:
+    port=int(input("ваш порт:"))
+    if not 0 <= port <= 65535:
+        raise ValueError
+except ValueError :
+    port = 9090
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', 9090))
-
+sock.setblocking(True)
+sock.connect(('localhost', port))
 msg = input()
-while msg!='exit':
- sock.send(msg.encode())
- msg= input()
-data = sock.recv(1024)
+while msg != 'exit':
+    sock.send(msg.encode())
+    #print(msg)
+    msg = input()
 
 sock.close()
-
-print(data.decode())
--- server.py
+++ server.py
@@ -1,23 +1,34 @@
 import socket
+from loger import Logfile
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+l = Logfile()
+l.serverstart()
+try:
+    port=int(input("Ваш порт:"))
+    if not 0 <= port <= 65535:
+        raise ValueError
+except ValueError:
+        port = 9090
+while True:
+    sock = socket.socket()
+    sock.bind(('', port))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    msg = ''
+    data=list()
+    print('')
+    while True:
+        findings = conn.recv(1024)
+        if findings:
+                data.append(findings.decode())
+                print(data[-1])
+        else:
+            conn.close()
+            break
 
-while True:
-    findings = conn.recv(1024)
-    if findings:
-        data.append(findings.decode())
-    else:
-        conn.close()
-        break
-data='\n'.join(data)
-print(data)
+print('\n'.join(data))
 
-print(msg)
 
 conn.close()
+l.serverend()
