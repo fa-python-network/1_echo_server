a1a392096f5fcf68fcab3f377f28147272826a7e
Polina Pavlova <paulinapvlv@gmail.com>
2019-09-26 22:19:37 +0300

    Выполнены задания № 1, 2, 3, 4, 5

--- client.py
+++ client.py
@@ -1,16 +1,46 @@
 import socket
-from time import sleep
+from datetime import datetime
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+def check_port(port):
+	default=9090
+	if type(port)=='int':
+		if 0<port<65536:
+			pass
+		else:
+			port=default
+	else:
+		port=default
+	return port
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def check_host(host):
+	if host=='localhost':
+		return host
+	else:
+		valid=True
+		l=host.split('.')
+		if len(l)==4:
+			for n in l:
+				if 0<=int(n)<=255:
+					pass
+				else:
+					valid=False
+					break
+		else:
+			valid=False
+	if not(valid):
+		host='localhost'
+	return host
 
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+host=check_host(input('Введите хост '))
+port=check_port(input('Введите номер порта '))
+msg=''
+while True:
+	s=socket.socket()
+	s.connect((host,port))
+	msg=input('Введите сообщение. Для выхода введите exit ')
+	if(msg=='exit'):
+		break
+	s.send(msg.encode())
+	f=open('log.txt','a')
+	f.write('Лог создан: {}. Отправлено сообщение {} длиной {} символов. Порт {}, хост {}\n'.format(datetime.today(),msg,len(msg),port,host))
+	f.close()
--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import socket
+s=socket.socket()
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def check_port(port):
+	default=9090
+	if type(port)=='int':
+		if 0<port<65536:
+			pass
+		else:
+			port=default
+	else:
+		port=default
+	return port
 
-msg = ''
+port=check_port(input('Введите номер порта '))
+s.bind(('',port))
+s.listen(1)
+
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
+	msg=''
+	conn,address=s.accept()
+	data=conn.recv(1024)
+	msg=data.decode()
+	print(msg)
 conn.close()
+	
