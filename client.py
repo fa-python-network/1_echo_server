4436b921450ead7f680fb4670d03c93558ea842e
Alina <kl.ali18@ya.ru>
2019-09-27 10:56:37 +0300

    1st

--- client.py
+++ client.py
@@ -1,16 +1,11 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+sock.connect(('localhost', 9090))
+msg=input()
+sock.send(msg)
 
 data = sock.recv(1024)
-
 sock.close()
 
-print(data.decode())
+print(data)
--- server.py
+++ server.py
@@ -2,19 +2,17 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
+sock.listen(1)
 conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+print ('connected:', addr)
+msg=''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
+    data = conn.recv(1024)
+    if not data:
+        break
+    msg+= data.decode()
+    conn.send(data.upper())
 print(msg)
-
 conn.close()
