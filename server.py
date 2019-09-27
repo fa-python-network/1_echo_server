f1165707c02f2fc5560f0ee564ee850be6e2da58
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-09-27 11:03:35 +0300

    цикл с выходом
    добавлена функция выхода при отключении

--- server.py
+++ server.py
@@ -1,20 +1,18 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
+sock.bind((''10.38.165.12'', 9090))
+sock.listen(4)
 conn, addr = sock.accept()
 print(addr)
 
-msg = ''
+data = 'new'
 
-while True:
+while data:
 	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	print(data.decode())
 
-print(msg)
 
-conn.close()
+
+
+
