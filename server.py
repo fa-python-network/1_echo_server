f5af845c1a846460ec1616793664dd62d4017a36
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-09-27 11:11:43 +0300

    Многослуховость
    Возможность слышать несколько чуваков

--- server.py
+++ server.py
@@ -3,14 +3,13 @@ import socket
 sock = socket.socket()
 sock.bind((''10.38.165.12'', 9090))
 sock.listen(4)
-conn, addr = sock.accept()
-print(addr)
-
-data = 'new'
-
-while data:
-	data = conn.recv(1024)
-	print(data.decode())
+while True
+	conn, addr = sock.accept()
+	print(addr)
+	data = 'new'
+	while data:
+		data = conn.recv(1024)
+		print(data.decode())
 
 
 
