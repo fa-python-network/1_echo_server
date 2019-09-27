dc4740a902a345042cd90cb7796b873a4231ce6b
kaktusss123 <komarnitskij.v@phystech.edu>
2019-09-27 15:22:00 +0300

    tasks comlete

--- client.py
+++ client.py
@@ -1,16 +1,26 @@
 import socket
 from time import sleep
+import logging as log
+
+log.basicConfig(format='%(filename)s [LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=log.DEBUG)
+
+ADDRESS, PORT = 'localhost', 9797
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+log.debug('Socket sterted')
+sock.connect((ADDRESS, PORT))
+log.info(f'Connected to {ADDRESS}:{PORT}')
 
-#msg = input()
-msg = "Hi!"
+msg = input('<= ')
+
+log.debug(f'Sending "{msg}"')
 sock.send(msg.encode())
 
+log.debug('Receiving data from server')
 data = sock.recv(1024)
 
+log.debug(f'Disconnecting from {ADDRESS}:{PORT}')
 sock.close()
 
-print(data.decode())
+print(f'=> {data.decode()}')
+# sock.close()
--- server.py
+++ server.py
@@ -1,20 +1,32 @@
 import socket
+import logging as log
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+log.basicConfig(format='%(filename)s [LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=log.DEBUG)
 
-msg = ''
+PORT = 9797
+MAX_CONN = 1
+running = True
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+with socket.socket() as sock:
+	log.debug('Socket created')
+	sock.bind(('', PORT))
+	log.debug(f'Socket binded to port {PORT}')
+	sock.listen(MAX_CONN)
+	log.debug(f'Socket is listening {MAX_CONN} connections')
+	while running:
+		conn, addr = sock.accept()
+		log.debug(f'Connected to {addr}')
+		while 1:
+			data = conn.recv(1024)
+			if not data:
+				log.debug(f'Client {addr} disconnected')
+				break
+			data = data.decode()
+			log.info(f'Received msg: {data}')
+			if data == 'exit':
+				log.debug('Received "exit"')
+				log.info('Server is closing')
+				running = False
+				conn.close()
+				break
+			conn.send(data.encode())
