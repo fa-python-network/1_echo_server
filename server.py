641cc3a9af5fabadccb8802f7f19e71aef278082
Андрей Баранюк <andrew-baranuk@yandex.ru>
2019-09-27 11:53:50 +0300

    3rd task

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 sock = socket.socket()
 sock.bind(('', 9090))
 print("Server started working")
-sock.listen(0)
+sock.listen(1)
 print("Server started listening")
 conn, addr = sock.accept()
 print("Connected client ", addr)
@@ -16,6 +16,15 @@ while True:
 	if not data:
 		print("Client didn't send data")
 		break
+	if data.decode() == "exit":
+		print("Client want to disconnect")
+		conn.close()
+		print("Disconnection client")
+		sock.listen(1)
+		print("Server started listening")
+		conn, addr = sock.accept()
+		print("Connected client ", addr)
+		continue
 	msg += data.decode()
 	print("Sending data to client")
 	conn.send(data)
