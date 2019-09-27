7ffc0a2828b5cb3732d1fa5311ec30c70e3e8e7b
Alina <kl.ali18@ya.ru>
2019-09-27 11:28:06 +0300

    second

--- client.py
+++ client.py
@@ -4,8 +4,10 @@ import socket
 sock = socket.socket()
 
 sock.connect(('localhost', 9090))
-msg = input()
-sock.send(msg.encode())
+msg=''
+while msg != 'exit':
+	msg = input()
+	sock.send(msg.encode())
 
 data = sock.recv(1024)
 print (data.decode())
