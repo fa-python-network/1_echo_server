d422245918b022f7709bb0e974fb8cb3a2502f76
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-09-27 11:31:40 +0300

    log files
    добавлены лог файлы

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 import socket
-
+import json
 sock = socket.socket()
-sock.bind((''10.38.165.12'', 9090))
+sock.bind(('10.38.165.12', 9090))
 sock.listen(4)
+command = ("exit")
 while True
 	conn, addr = sock.accept()
 	print(addr)
 	data = 'new'
 	while data:
 		data = conn.recv(1024)
-		print(data.decode())
+		data = data.decode()
+		if data not in command:
+			print(data)
+		else:
+			with open("log_serv.json", "a") as file:
+				json.dump(data, file)
+			
 
 
 
