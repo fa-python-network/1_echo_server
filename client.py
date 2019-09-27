f46b66c8a52121ca9ed2489bd62605a2965a9ec1
Калугина Евгения Алексеевна <185371@edu.fa.ru>
2019-09-27 09:46:31 +0300

    Задания 1 - 5

--- client.py
+++ client.py
@@ -1,12 +1,29 @@
 import socket
 from time import sleep
 
+host = input('Введите хост: ')
+while True:
+	port = input('Введите порта: ')
+	try:
+		port = int(port)
+		if 100 <= port <= 65535:
+			break
+		print("Неправильный диапазон")
+	except ValueError:
+		print("Неправильный порт")
+		
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect((host, port))
 
-#msg = input()
-msg = "Hi!"
+msg = ""
+while True:
+	request_msg = input("Введите сообщение: ")
+	if request_msg == "exit":
+		break
+	else:
+		msg += "\n" + request_msg
+	
 sock.send(msg.encode())
 
 data = sock.recv(1024)
--- server.py
+++ server.py
@@ -1,20 +1,44 @@
 import socket
+from datetime import datetime
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def log(data):
+	with open("logs.log", "a") as file1:
+		print(str(datetime.now())+ " | " + data, file=file1)
 
-msg = ''
+
+
+host = input('Введите хост: ')
+while True:
+	port = input('Введите порта: ')
+	try:
+		port = int(port)
+		if 100 <= port <= 65535:
+			break
+		print("Неправильный диапазон")
+	except ValueError:
+		print("Неправильный порт")
+log(f"Запуск сервера")
+sock = socket.socket() 
+sock.bind((host, port)) 
+sock.listen(1)
+log(f"Начало прослушивания порта")
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	
+	conn, addr = sock.accept()
+	log(f"Клиент {addr[0]}:{addr[0]} подключился")
 
-print(msg)
+	msg = ''
+
+	while True: 
+		data = conn.recv(1024) 
+		log(f"Получает новые данные от пользователя {addr[0]}:{addr[0]}")
+		if not data: 
+		msg += data.decode("utf-8") 
+		conn.send(data) 
+		log(f"Отправка '{data}' пользователю {addr[0]}:{addr[0]}")
+	log(f"Пользователь {addr[0]}:{addr[0]} отключился")
+	print(msg) 
 
 conn.close()
+log("Сервер выключился")
