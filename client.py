4f79e319b795f2b721506fcdad180d08a6fbcd27
Alina <kl.ali18@ya.ru>
2019-09-27 11:12:54 +0300

    2st

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
+
 import socket
 
 sock = socket.socket()
+
 sock.connect(('localhost', 9090))
-msg=input()
-sock.send(msg)
+msg = input()
+sock.send(msg.encode())
 
 data = sock.recv(1024)
+print (data.decode())
 sock.close()
 
-print(data)
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(1)
+sock.listen(0)
 conn, addr = sock.accept()
 
 print ('connected:', addr)
