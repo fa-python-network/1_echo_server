d07b83cd5fef53215ba162ebf7f1e6de8f48b750
Liliya <lili.rachkova@ya.ru>
2019-09-27 12:25:47 +0300

    n4

--- client.py
+++ client.py
@@ -2,12 +2,27 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.connect(('localhost', 9090))
+k = False
+while k==False:
+	try:
+		print("host:")
+		host = input()
+		if host == "":
+			host = 'localhost'
 
-msg = input()
-while msg!='exit':
-	sock.send(msg.encode())
-	msg = input()
+		print("port:")
+		port = input()
+		if port == "":
+			port = 9090
+		
+		sock.connect((host, int(port)))
+		msg = input()
+		while msg!='exit':
+			sock.send(msg.encode())
+			msg = input()
+		k=True
+	except:
+		print("wrong host or port")
 	#data = sock.recv(1024)
 
 #print(data.decode())
