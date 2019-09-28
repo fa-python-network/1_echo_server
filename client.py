365049a532f0545380ab673952601680675004a1
Alina <54978589+Meithnrm@users.noreply.github.com>
2019-09-28 11:27:51 +0300

    add first five tasks
    Первые пять задач

--- client.py
+++ client.py
@@ -1,16 +1,33 @@
 import socket
-from time import sleep
+
+
+host = input("Input host address IPv4 or localhost, DEFAULT - 127.0.0.1: ")
+host_list = host.split('.')
+if host == 'localhost' and 0 < int(host_list[0]) <= 255 and 0 < int(host_list[1]) <= 255 and 0 < int(host_list[2]) <= 255 and 0 < int(host_list[3]) <= 255:
+    pass
+else:
+    print("Data is not correct. Host is 127.0.0.1")
+    host = '127.0.0.1'
+
+port = input("Input port. Default - 9090: ")
+
+try:
+    port = int(port)
+    if 1024 < port <= 10000:
+        pass
+    else:
+        print("Data is not correct. Port is 9090")
+        port = 9090
+except:
+    print("Data is not correct. Port is 9090")
+    port = 9090
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect((host, port))
+msg = ''
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+while msg.lower().strip() != "exit":
+    msg = input(' -> ')
+    sock.send(msg.encode())
+    data  =sock.recv(1024)
+    print(data.decode())
