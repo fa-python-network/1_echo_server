ce96818e621beab452eee2fc051128e725ebcaa1
Vlad <vlad07020@mail.ru>
2019-09-28 15:55:12 +0300

    zadanie 5

--- server1.py
+++ server1.py
@@ -1,26 +1,38 @@
+import logging
+
+server1_log = logging.FileHandler('server1.log')
+console_out = logging.StreamHandler()
+
+logging.basicConfig(handlers=(server1_log, console_out),
+format='[%(asctime)s | %(levelname)s]: %(message)s',
+datefmt = '%m.%d.%Y %H:%M:%S',
+level = logging.INFO)
+
+
+
 import socket
 
 sock = socket.socket()
 port = 9080
 sock.bind(('', port))
-print("Запустили сервер")
+logging.info("Запустили сервер")
 
 sock.listen(1)
-print("Начинаем прослушивать порт")
+logging.info("Начинаем прослушивать порт")
 while True:
 	conn, addr = sock.accept()
-	print(f"Подключение клиента с адрессом {addr}")
+	logging.info(f"Подключение клиента с адрессом {addr}")
 
-	print("Прием данных от клиента")
+	logging.info("Прием данных от клиента")
 	msg=''
 	while True:
 		data = conn.recv(1024)
 		if not data:
 			break
 		msg+=data.decode()
-	print (msg)
+	logging.info(msg)
 
 	conn.close()
 
-print("Отключение клиента")
-print("Остановка сервера")
+logging.info("Отключение клиента")
+logging.info("Остановка сервера")
