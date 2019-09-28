fb0701a54ec1eeb2d257a76b248356c164a9522b
Vlad <vlad07020@mail.ru>
2019-09-28 15:10:43 +0300

    zadanie_3_i_4

--- client1.py
+++ client1.py
@@ -1,21 +1,36 @@
 import socket
 
 sock = socket.socket()
-port = 9080
-sock.connect(('localhost', port))
-msg = ''
-a = "exit"
+port_standart = 9080
+server_standart = 'localhost'
+
+
+server = input("Введите сервер ")
+server1 = server.split(".", 4)
+for el in server1:
+	el = int(el)
+	if 0<=el<=255:
+		pass
+	else:
+		server = server_standart
+
+port = input("Введите номер порта ")
+if 1024<=int(port)<=65535:
+	pass
+else:
+	port = port_standart
+
+
+sock.connect((server, port))
 msg2 = ''
 while True:
 	msg = input()
-	if msg == a:
+	if msg == "exit":
 		break
 	msg2 = msg2 + msg
 
 
-#msg = "Hello, server. I'm client"
 sock.send(msg2.encode())
 
-#responce = sock.recv(1024)
+
 sock.close()
-#print(responce.decode())
--- server1.py
+++ server1.py
@@ -7,22 +7,20 @@ print("Запустили сервер")
 
 sock.listen(1)
 print("Начинаем прослушивать порт")
-
-conn, addr = sock.accept()
-print(f"Подключение клиента с адрессом {addr}")
-
-print("Прием данных от клиента")
-msg=''
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg+=data.decode()
-print (msg)
+	conn, addr = sock.accept()
+	print(f"Подключение клиента с адрессом {addr}")
 
-msg = "Данные получены!"
-conn.send(msg.encode())
-conn.close()
+	print("Прием данных от клиента")
+	msg=''
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg+=data.decode()
+	print (msg)
+
+	conn.close()
 
 print("Отключение клиента")
 print("Остановка сервера")
