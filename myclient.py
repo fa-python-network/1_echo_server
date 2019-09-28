c0591e5710cdc8b54e02f29d8743126e9cc267c0
nastya <pin-2169@rambler.ru>
2019-09-28 12:52:00 +0300

    Client wants to exit

--- myclient.py
+++ myclient.py
@@ -6,15 +6,20 @@ port=8080
 sock.connect(('localhost', port))
 print(f'Клиент подлкючился к серверу {port}')
 
-print('Введите текст сообщения')
-data = input()
-
-print('Отправка сообщения')
-sock.send(data.encode())
-
-ans=sock.recv(1024) #ответ от сервера
-
+while True:
+	print('Введите текст сообщения')
+	data = input()
+	if data == 'exit':
+		break
+	print('Отправка сообщения')
+	sock.send(data.encode())
+	ans=sock.recv(1024) #ответ от сервера
+	print('Сервер передал вам сообщение')
+	print(ans.decode()) # вывод ответа в консоль
+	
 sock.close()
 
-print(ans.decode()) # вывод ответа в консоль
+
+#print('Сервер передал вам сообщение')
+#print(ans.decode()) # вывод ответа в консоль
 
