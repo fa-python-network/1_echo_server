78cfb7f6e49b151e70c543b2479cb3beeba5042a
Anton <anton.reient@gmail.com>
2019-09-28 19:03:57 +0300

    Applied all required modifications

--- client.py
+++ client.py
@@ -1,16 +1,28 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def settings():
+	"""
+	Настройка подключения к серверу
+	"""
+	host_prompt = input('Type the HOST if needed: ')
+	port_prompt = input('Type the PORT if needed: ')
 
-data = sock.recv(1024)
+	global HOST
+	global PORT
+	HOST = '127.0.0.1' if not host_prompt else host_prompt
+	PORT = 9014 if not port_prompt else int(port_prompt)
 
-sock.close()
 
-print(data.decode())
+if __name__ == '__main__':
+    settings()
+
+    with socket.socket() as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'Hi, dingus')
+
+        while True:
+            data = s.recv(1024)
+            s.sendall(bytes(input(), 'utf-8'))
+
+    print(f'Received {data}')
--- server.py
+++ server.py
@@ -1,20 +1,49 @@
 import socket
+import time
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+def settings():
+	"""
+	Настройка сервера
+	"""
+	host_prompt = input('Type the HOST if needed: ')
+	port_prompt = input('Type the PORT if needed: ')
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	global HOST
+	global PORT
+	HOST = '127.0.0.1' if not host_prompt else host_prompt
+	PORT = 9014 if not port_prompt else int(port_prompt)
 
-print(msg)
 
-conn.close()
+def write_to_file(msg: str, filename='log.txt'):
+	"""
+	Записівает сообщения в файл
+	"""
+	with open(filename, 'a') as f:
+		f.write(f'-----------------------\ntime: {time.time()}\nmsg: {msg}\n')
+
+
+if __name__ == '__main__':
+	settings()
+
+	with socket.socket() as s:
+		s.bind((HOST, PORT))
+		s.listen()
+
+		while True:
+			conn, addr = s.accept()
+
+			write_to_file('Accepted a new request')
+
+			with conn:
+				write_to_file(f'Connected by {addr}')
+
+				while True:
+					data = conn.recv(1024)
+					write_to_file(data.decode())
+
+					if not data or data == b'exit':
+						write_to_file('Stopped serving a user')
+						break
+
+					conn.sendall(data)
