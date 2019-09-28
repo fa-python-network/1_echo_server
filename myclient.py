3c1b842474a65965f8fc08c8fd660f26ca653c77
nastya <pin-2169@rambler.ru>
2019-09-28 13:29:13 +0300

    Enter IP and port

--- myclient.py
+++ myclient.py
@@ -2,8 +2,25 @@ import socket
 
 sock = socket.socket()
 
-port=8080
-sock.connect(('localhost', port))
+host = input("Введите адрес хоста: " )
+host1 = host.split('.')
+for char in host1:
+	if 0<=int(char)<=255:
+		pass
+	else:
+		print("Incorrect IP")
+		host = 'localhost'
+
+port = int(input("Введите номер порта: "))
+if 1024<=port<=65535:
+	pass
+else:
+	print("Incorrect port")
+	port = 8080
+
+
+
+sock.connect((host, port))
 print(f'Клиент подлкючился к серверу {port}')
 
 while True:
