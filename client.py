a38406c04f636f236a7bb661fb3e6d47fe82c8f8
Nick <niki150700@yandex.ru>
2019-09-29 16:48:47 +0100

    Задание №3 и №4

--- client.py
+++ client.py
@@ -2,13 +2,32 @@ import socket
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9090))
+
+
+host = input("Введите адрес хоста: ")
+if host == "localhost":
+	pass
+else:
+	hostls = host.split(".", 4)
+	for i in hostls:
+		if 0 <= int(i) <= 255:
+			pass
+		else:
+			host = 'localhost'
+port = int(input("Введите адрес порта: "))
+if 1024 <= int(port) <= 65535:
+	pass
+else:
+	port = 9000
+
+sock.connect((host, port))
 
 msg = ""
 while True:
 	cl_input = input()
 	if cl_input == "exit":
 		break
+		sock.close
 	msg += cl_input + " "
 sock.send(msg.encode())
 
--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
+sock.bind(('', 9000))
 sock.listen(1)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+	try:
+		conn, addr = sock.accept()
+	except sock.error:
+		pass
+	msg = ''
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg+=data.decode()
+		conn.send(data)
+	print(msg)
 
 conn.close()
