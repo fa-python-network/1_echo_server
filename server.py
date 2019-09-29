57978c497261ecfb05ad1cbefa6a3bba23ee4258
1 <1@mail.com>
2019-09-29 22:33:29 +0300

    3 task

--- server.py
+++ server.py
@@ -3,18 +3,20 @@ import socket
 sock = socket.socket()
 sock.bind(('', 9093))
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	
+	conn, addr = sock.accept()
+	print(addr)
 
-print(msg)
+	msg = ''
 
-conn.close()
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg += data.decode()
+		conn.send(data)
+
+	print(msg)
+
+	conn.close()
