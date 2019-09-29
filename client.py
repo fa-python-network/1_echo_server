252f7fb3b4139bdeeed4d5f2941ff537699a5b9a
Albert <albert5162@mail.ru>
2019-09-29 14:59:12 +0300

    3 и 4 задание

--- client.py
+++ client.py
@@ -1,9 +1,55 @@
 import socket
 from time import sleep
 
+
+def get_port():
+    port = int(input('Введите порт от 1024 до 65535: \n'))
+    if 1023 < port < 65535:
+        return port
+    print('Не валидный порт')
+    return get_port()
+
+def get_ip():
+    ip = input('Введите ip (10.10.10.10) или оставьте пусстым для значения localhost: \n')
+
+    if ip == '':
+        return 'localhost'
+
+    ip = ip.split('.')
+
+    if len(ip) != 4:
+        print('Не валидный ip')
+        return get_ip()
+
+    ip = list(map(lambda x: int(x), ip))
+
+    for i in ip:
+        if 0 <= i <= 255:
+            continue
+        else:
+            print('Не валидный ip')
+            return get_ip()
+    return '.'.join(list(map(lambda x: str(x), ip)))
+
+
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9090))
+
+
+while True:
+    ips = get_ip()
+    port = get_port()
+    try:
+        print('Ожидаем соединения')
+        sock.connect((ips, port))
+        break
+    except BaseException:
+        print('Возникла ошибка соединения')
+        sock.close()
+        sock = socket.socket()
+        sock.setblocking(1)
+
+print('Соединение установлено')
 
 msg = ""
 input_date = input()
@@ -17,5 +63,3 @@ if msg != "":
     print(data.decode())
 
 sock.close()
-
-
--- server.py
+++ server.py
@@ -1,20 +1,34 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
-print(addr)
+def get_port():
+	port = int(input('Введите порт от 1024 до 65535: \n'))
+	if 1023 < port < 65535:
+		return port
+	print('Не валидный порт')
+	return get_port()
 
-msg = ''
+sock = socket.socket()
+
+port = get_port()
+
+sock.bind(('', port))
+print('Сервер запущен на порту: ', port)
+
+sock.listen(1)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	conn, addr = sock.accept()
+	print(addr)
 
-print(msg)
+	msg = ''
 
-conn.close()
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg += data.decode()
+		conn.send(data)
+
+	print(msg)
+
+	conn.close()
