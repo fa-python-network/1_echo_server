a658629ba9e7788eb25201bfaf2e49a2adf2e039
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-09-29 18:19:36 +0300

    some improvements and Task 3

--- client.py
+++ client.py
@@ -5,10 +5,11 @@ sock = socket.socket()
 sock.setblocking(1)
 sock.connect(('localhost', 9090))
 
+
 while True:
     msg = input("Input message: ")
     if msg == "exit":
-        data = sock.recv(1024)
         sock.close()
         break
     sock.send(msg.encode())
+    data = sock.recv(1024)
