279cdfaff070123ad9c16f0c20c946d84dfce6d0
silverdoggos <55032919+silverdoggos@users.noreply.github.com>
2019-09-29 14:02:05 +0300

    Task № IV

--- client.py
+++ client.py
@@ -1,9 +1,33 @@
 import socket
 from time import sleep
-
+import re
+while True:
+    inputPort = input('Vvedite port nomber or \'Ok\' if you want port 9090: ')
+    if inputPort == 'Ok':
+        print('Ok')
+        inputPort = 9090
+        break
+    elif not inputPort.isnumeric():
+        print('try again')
+    elif not  1024 < int(inputPort) <= 65535:
+        print('try again')
+    else:
+        inputPort = int(inputPort)
+        print('great')
+        break
+while True:
+    inputHost = input('Vvedite host or \'Ok\' if you want localhost: ')
+    if inputHost == 'Ok':
+        print('Ok')
+        inputHost = 'localhost'
+        break
+    elif re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', inputHost) == None:
+       print('try again')
+    else:
+        break
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 7003))
+sock.connect((inputHost, inputPort))
 print("Connection with server")
 data = b''
 while True:
--- server.py
+++ server.py
@@ -1,19 +1,18 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 7003))
+sock.bind(('', 9091))
 print("Server starts")
 sock.listen(0)
-
 print("Now listen")
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    msg += data.decode()
-    conn.send(data)
+    conn, addr = sock.accept()
+    print("new connection: " + f"{addr}")
 
+    msg = ''
+
+    while True:
         data = conn.recv(1024)
         print("new data from client")
         if not data:
