782697d248ba086c4e1693a5a443b7420c3ab3ce
Сергей Ефремов <yefr@MacBook-Pro-Sergej-3.local>
2019-09-30 01:46:15 +0300

    Zadanie 5

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 import socket
 
+port = 9089
+file = open("logfile.txt", "a")
 sock = socket.socket()
-sock.bind(('', 9089))
+sock.bind(('', port))
+file.write(f'Сервер использует порт{port}\n')
 sock.listen(1)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+file.write(f'Сервер слушает клиента{port}\n')
 while True:
 	try:
 		conn, addr = sock.accept()
@@ -15,11 +14,13 @@ while True:
 		pass
 	msg = ''
 	while True:
+		file.write(f'Сервер получает данные{addr}\n')
 		data = conn.recv(1024)
 		if not data:
 			break
 		msg+=data.decode()
 		conn.send(data)
-	print(msg)
-
+		print(msg)
+	file.write(f'Сервер отвечает\n')
+file.close()
 conn.close()
