23d578ca169175cea281986dca37a37ad6d2faaf
natashagushan <natysya.gushan@gmail.com>
2019-09-30 00:03:46 +0200

    Zadaniya

--- server.py
+++ server.py
@@ -1,5 +1,17 @@
 import socket
+from re import match
 sock = socket.socket()
+host = 'localhost'
+port = 9090
+host_ = input('Host: ')	
+port_ = input('Port:')
+host_regex = r'[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]'
+if host_ and match(host_regex, host_):
+    host = host_
+print(f'current host:{host}')
+if port_.isdigit() and  1024<=int(port_)<=65535:
+	port = int(port_)
+print(f'current port: {port}')
 sock.bind(("",9094))
 sock.listen(5)
 while True:
