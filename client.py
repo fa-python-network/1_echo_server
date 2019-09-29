97184cf48c0da2f3dcd60ff3a799e54ebd616378
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-09-29 18:59:33 +0300

    task 4

--- client.py
+++ client.py
@@ -1,10 +1,29 @@
 import socket
+import re
 from time import sleep
 
+while True:
+    port = input('Введите порт от 1024 до 65535: \n')
+    if not port.isnumeric():
+        print('Ошибка')
+    elif 1023 <= int(port) <= 65535:
+        break
+    else:
+        print('Ошибка: порт не входит в нужный диапазон')
+
+while True:
+    ip = input('Введите ip сервера или оставьте пусстым для значения localhost: \n')
+    if ip == '':
+        ip = 'localhost'
+        break
+    elif re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip) == None:
+        print('Ошибка')
+    else:
+        break
+    
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9090))
-
+sock.connect((ip, int(port)))
 
 while True:
     msg = input("Input message: ")
--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
+while True:
+    port = input('Введите порт от 1024 до 65535: \n')
+    if not port.isnumeric():
+        print('Ошибка')
+    elif 1023 <= int(port) <= 65535:
+        break
+    else:
+        print('Ошибка: порт не входит в нужный диапазон')
+sock.bind(('', int(port)))
 sock.listen(0)
 conn, addr = sock.accept()
 print(addr)
