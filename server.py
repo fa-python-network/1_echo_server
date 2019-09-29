35d634f5905d001dd80a79c9d515d156d54a9fc8
Nick <niki150700@yandex.ru>
2019-09-29 17:23:22 +0100

    Задание №5

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import socket
 
+txt = open('log.txt', 'w')
 sock = socket.socket()
 sock.bind(('', 9000))
+print("Начало работы сервера", file = txt)
 sock.listen(1)
+print("Идёт прослушивание", file = txt)
 
 while True:
 	try:
 		conn, addr = sock.accept()
+		print(f"Новое подключение: {addr}", file = txt)
 	except sock.error:
 		pass
 	msg = ''
