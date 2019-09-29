65fb85e089ebbc8f1126490ed66f4c5764a35eaa
Viktoriya <v.7z@mail.ru>
2019-09-29 09:40:47 +0300

    Четвертое задание

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('127.0.0.1', 8975))
+while True:
+	host = input('Введите адрес хоста или localhost : \n')
+	if host=='localhost':
+		host='127.0.0.1'
+		break
+	host_l=host.split('.')
+	if (0<int(host_l[0])<255) and (0<int(host_l[1])<255) and (0<int(host_l[2])<255) and (0<int(host_l[3])<255):
+		break
+	else:
+		print('Введен неверный формат адреса.')
+while True:
+	port=input('Введите номер порта от 1024 до 49151: \n')
+	if 1023<int(port)<49152:
+		break
+	else:
+		print('Неверный номер порта.')
+		break
+sock.connect((host, int(port)))
 msg = ""
 while msg != 'exit':
 	msg=input('Enter your message: ')
--- server.py
+++ server.py
@@ -1,7 +1,24 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 8975))
+while True:
+	host = input('Введите адрес хоста или localhost : \n')
+	if host=='localhost':
+		host='127.0.0.1'
+		break
+	host_l=host.split('.')
+	if (0<int(host_l[0])<255) and (0<int(host_l[1])<255) and (0<int(host_l[2])<255) and (0<int(host_l[3])<255):
+		break
+	else:
+		print('Введен неверный формат адреса.')
+while True:
+	port=input('Введите номер порта от 1024 до 49151: \n')
+	if 1023<int(port)<49152:
+		break
+	else:
+		print('Неверный номер порта.')
+		break
+sock.bind(('', int(port)))
 sock.listen(1)
 
 
