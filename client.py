bb61d69739da08544261adf7088d6d559a98871d
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-09-29 17:06:16 +0300

    Пункт 7

--- client.py
+++ client.py
@@ -6,6 +6,10 @@ sock.setblocking(True)
 port =  int(input("Порт:"))
 port = port if (port >= 0 and port <= 65535)  else  9090
 sock.connect((input('Имя хоста:'), port))
+user = sock.recv(1024).decode()
+if "знаю" in user:
+    name = input(user)
+    sock.send(name.encode())
 msg = input()
 while msg != 'exit':
     sock.send(msg.encode())
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import socket
 from datetime import datetime
 from contextlib import closing
+import os
 class Server():
-	def __init__(self, log = "file.log"):
+	def __init__(self, log = "file.log", users = "users.txt"):
 		self.__log = log
+		self.__users = users
 		self.port =  int(input("Порт:"))
 	@property
 	def startServer(self):
@@ -14,8 +16,9 @@ class Server():
 			sock.bind(('', self.port))
 			sock.listen(1)
 			conn, addr = sock.accept()
+			print(self.checkUser(addr, conn))
 			self.serverStarted(addr)
-			data=list()
+			data = list()
 			while True:
 				msg = conn.recv(1024)
 				if msg:
@@ -37,6 +40,24 @@ class Server():
 			except:
 				pass
 		assert False, "Порт не найден"
+	
+	def checkUser(self, addr, conn):
+		try:
+			open(self.__users).close()
+		except FileNotFoundError:
+			open(self.__users, 'a').close()
+		with open(self.__users, "r", encoding="utf-8") as f:
+			d = dict(x.rstrip().split(None, 1) for x in f)
+			try:
+				name = d[str(addr[0])]
+				conn.send("OK".encode())
+			except KeyError:
+				conn.send("Привет. Я тебя не знаю. Скажи мне свое имя:".encode())
+				name = conn.recv(1024).decode()
+				with open(self.__users, "a", encoding="utf-8") as s:
+					print(f"{str(addr[0])} {name}", file=s)
+		return f"Здравствуйте, {name}"
+
 	def serverStarted(self,ip):
 		with open(self.__log, "a", encoding="utf-8") as f:
 			print(f"{datetime.now().time()} Server Launched {ip}", file=f)
