7d9d2ef92fa37020e1c07740d85250c8b6061e12
соня <sonyakt@bk.ru>
2019-09-29 21:47:21 +0500

    задание 3 и 4

--- client.py
+++ client.py
@@ -1,17 +1,36 @@
 import socket
+import re 
 from time import sleep
+
+while True:
+	port = int(input('укажите порт в диапазоне 1024-65535: \n))
+	if 1024<= port <= 65535:
+		break
+	else:
+		print('ошибка, введите порт снова')
+
+while True:
+	ip= input("введите ip или _ для localhost: \n")
+	if ip =='':
+		ip= 'localhost'
+		break
+	elif re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip) == None:
+		print('введите снова')
+	else:
+		break
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9091))
+sock.connect((ip, int(port)))
 
 while True:
 	msg = input()
 	if msg == "exit":
-		data = sock.recv(1024)
 		sock.close()
+		print('stop')
 		breake
 	sock.send(msg.encode())
+	data+= sock.recv(1024)
 
-
-print(data.decode())
+if data:
+	print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9091))
+print("start")
+
+
+while True:
+	port = int(input('укажите порт в диапазоне 1024-65535: \n))
+	if 1024<= port <= 65535:
+		break
+	else:
+		print('ошибка, введите порт снова')
+sock.blind(('',int(port)))
 sock.listen(0)
-conn, addr = sock.accept()
+conn,addr = sock.accept()
 print(addr)
 
 msg = ''
-
 while True:
 	data = conn.recv(1024)
 	if not data:
 		break
 	msg += data.decode()
 	conn.send(data)
-
 print(msg)
-
 conn.close()
+        
+
