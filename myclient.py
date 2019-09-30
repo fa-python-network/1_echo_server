326825b710ea02152ba0556366ea0ee441872a37
nastya <pin-2169@rambler.ru>
2019-09-30 17:46:19 +0300

    Identification

--- myclient.py
+++ myclient.py
@@ -2,39 +2,47 @@ import socket
 
 sock = socket.socket()
 
-host = input("Введите адрес хоста: " )
-host1 = host.split('.')
-for char in host1:
-	if 0<=int(char)<=255:
-		pass
-	else:
-		print("Incorrect IP")
-		host = 'localhost'
+#host = input("Введите адрес хоста: " )
+#host1 = host.split('.')
+#for char in host1:
+	#if 0<=int(char)<=255:
+		#pass
+	#else:
+		#print("Incorrect IP")
+		#host = 'localhost'
 
-port = int(input("Введите номер порта: "))
-if 1024<=port<=65535:
-	pass
-else:
-	print("Incorrect port")
-	port = 8080
+#port = int(input("Введите номер порта: "))
+#if 1024<=port<=65535:
+	#pass
+#else:
+	#print("Incorrect port")
+	#port = 8080
 
-
-
-sock.connect((host, port))
+port = 8080
+sock.connect(('localhost', port))
+#sock.connect((host, port))
 print(f'Клиент подлкючился к серверу {port}')
 
-while True:
-	print('Введите текст сообщения')
-	data = input()
-	if data == 'exit':
-		break
-	print('Отправка сообщения')
-	sock.send(data.encode())
-	ans=sock.recv(1024) #ответ от сервера
+#while True:
+	#print('Введите текст сообщения')
+	#data = input()
+	#if data == 'exit':
+		#break
+	#print('Отправка сообщения')
+	#sock.send(data.encode())
+	#ans=sock.recv(1024) #ответ от сервера
+
+ans=sock.recv(1024) #ответ от сервера
+if ans.decode() == 'send me your name':
+	print("Сервер запрашивает ваше имя, введите его")
+	name = input()
+	sock.send(name.encode())
+else:
+	print(ans.decode())
 
 sock.close()
-print('Сервер передал вам сообщение')
-print(ans.decode()) # вывод ответа в консоль
+#print('Сервер передал вам сообщение')
+#print(ans.decode()) # вывод ответа в консоль
 
 
 #print('Сервер передал вам сообщение')
--- myserver.py
+++ myserver.py
@@ -9,20 +9,24 @@
 
 
 import socket
+import os
+import pickle
 
 sock = socket.socket()
 
 port = 8080
+client_base='serverclients.pickle'
+ourclients={}
 
 #меняем порт, если он занят
-while True:
-	try:
-		sock.bind(('', port))
-		break
-	except:
-		port+=1
+#while True:
+#	try:
+#		sock.bind(('', port))
+#		break
+#	except:
+#		port+=1
 
-
+sock.bind(('', port))
 print(f'Сервер подключился к порту {port}')
 #logging.info(f'Сервер подключился к порту {port}')
 
@@ -31,19 +35,46 @@ sock.listen(count_of_clients)
 print(f'Сервер готов к прослушиванию')
 #logging.info(f'Сервер готов к прослушиванию')
 
+if os.stat(client_base).st_size>0:
+	with open(client_base, 'rb') as file:
+		ourclients=pickle.load(file)
+
+#print("Данные о клиентах успешно записаны")
+
 while True:
 	conn, addr = sock.accept()
-	print(f'Сервер подключился к клиенту {addr}')
-	#logging.info(f'Сервер подключился к клиенту {addr}')
 	while True:
-		data = conn.recv(1024)
-		if not data:
-			break
-		conn.send(data)
-		print('Отправка сообщения клиенту')
+		if addr[0] in ourclients.keys():
+			mess = "Hello," + ourclients[addr[0]] + "!!!"
+			conn.send(mess.encode())
+		else:
+			conn.send("send me your name".encode())
+			#получаем имя клиента
+			newname = conn.recv(1024)
+			msg = conn.send("Thank you, I got your name".encode())
+			ourclients.update({addr[0]:newname.decode()})
+			with open (client_base, 'wb') as f1:
+				pickle.dump(ourclients, f1)
+		break
+
+print(ourclients.keys())
+
+conn.close()
+print('Соединение завершено')
+
+#while True:
+#	conn, addr = sock.accept()
+#	print(f'Сервер подключился к клиенту {addr}')
+	#logging.info(f'Сервер подключился к клиенту {addr}')
+#	while True:
+#		data = conn.recv(1024)
+#		if not data:
+#			break
+#		conn.send(data)
+#		print('Отправка сообщения клиенту')
 		#logging.info('Отправка сообщения клиенту')
-	conn.close()
-	print('Соединение завершено')
+#	conn.close()
+#	print('Соединение завершено')
 	#logging.info('Соединение завершено')
 
 
