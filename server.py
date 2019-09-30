46e4ea35d89ff54b5a269a09283d38e88f955aa3
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-09-30 14:54:28 +0300

    task 6

--- server.py
+++ server.py
@@ -2,20 +2,21 @@ import socket
 
 f = open ('log.txt', 'w')
 sock = socket.socket()
+port = 9090
 while True:
-    port = input('Введите порт от 1024 до 65535: \n')
-    if not port.isnumeric():
-        print('Ошибка')
-    elif 1023 <= int(port) <= 65535:
-        break
-    else:
-        print('Ошибка: порт не входит в нужный диапазон')
-sock.bind(('', int(port)))
+	try:
+		if (port == 65536):
+			print('Все порты заняты')
+			break
+		sock.bind(('', port))
+		break
+	except:
+		port += 1
+print(f'Port number: {port}')
 print("Server starts", file = f)
 sock.listen(0)
 print("Now listen", file = f)
 conn, addr = sock.accept()
-print(addr)
 
 msg = ''
 
@@ -32,3 +33,4 @@ print(msg)
 
 conn.close()
 print('stop client', file = f)
+f.close
