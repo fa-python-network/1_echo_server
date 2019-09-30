be227529a6466c8329568668b0020511a98f41f8
root <vmolchanovag@mail.ru>
2019-09-30 03:39:04 +0300

    task 4 done

--- client.py
+++ client.py
@@ -1,16 +1,40 @@
 import socket
+import sys
 from time import sleep
+
+while True:
+	client_host = input('enter your host: ' )
+	if client_host=='localhost':
+		break
+	else:
+		parts=client_host.split('.',4)
+		for i in parts:
+			if 0<=int(i)<=255:
+				break
+			else:
+				print('mistake')
+
+while True:
+	client_port=int(input('enter your port: '))
+	if 1024<=client_port<=65525:
+		break
+	else:
+		print('wrong port... try again')
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect((client_host, client_port))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+msg = ''
+while True:
+	client_msg=input()
+	if 'exit' in client_msg:
+		sock.close()
+		break
+	msg+=client_msg+' '
+	sock.send(msg.encode())
 
 data = sock.recv(1024)
-
-sock.close()
-
 print(data.decode())
+
+
--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 
+while True:
+	port = int(input('enter your port: '))
+	if 1024<=port<=65525:
+		break
+	else:
+		print('wrong port...')
+
 sock = socket.socket()
-sock.bind(('', 9090))
+sock.bind(('',port))
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+
+while True:
+	conn,addr=sock.accept()
+	print(addr)
 
 msg = ''
-
 while True:
 	data = conn.recv(1024)
 	if not data:
 		break
-	msg += data.decode()
-	conn.send(data)
-
+	msg = data.decode()
+conn.send(data)
 print(msg)
-
 conn.close()
