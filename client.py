159d036829e45d9d17c50c0bf0c6c3a6dae21538
Anton Gabov <toxicarchont@gmail.com>
2019-09-30 23:51:00 -0400

    zadanie 4

--- client.py
+++ client.py
@@ -1,9 +1,39 @@
 import socket
 from time import sleep
+import re
 
+
+def checker():
+    port = "4000"
+    host = "localhost"
+    print("default host is 127.0.0.1(localhost)\n"
+          "default port is 4000"
+          )
+    while True:
+        uhost = input("vvedite host:\n")
+        if uhost == "default" or uhost == "localhost":
+            break
+        elif re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', uhost) == None:
+            print("try again")
+        else:
+            host = uhost
+            break
+    while True:
+        uport = input("vvedite port:\n")
+        if uport == "default" or uport == "4000":
+            break
+        elif not uport.isnumeric() == True or int(uport)<1024 or int(uport) > 65535:
+            print("try again")
+        else:
+            port = uport
+            break
+    return host, int(port)
+
+
+res = checker()
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9091))
+sock.connect(res)
 
 while True:
 
@@ -13,8 +43,8 @@ while True:
 	if "exit" in msg:
 		break
 
-	data = sock.recv(1024)
+	# data = sock.recv(1024)
 
 sock.close()
 
-print(data.decode())
+# print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,50 @@
 import socket
+import re
 
+
+def checker():
+    port = "4000"
+    host = "localhost"
+    print("default host is 127.0.0.1(localhost)\n"
+          "default port is 4000"
+          )
+    while True:
+        uhost = input("vvedite host:\n")
+        if uhost == "default" or uhost == "localhost":
+            break
+        elif re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', uhost) == None:
+            print("try again")
+        else:
+            host = uhost
+            break
+    while True:
+        uport = input("vvedite port:\n")
+        if uport == "default" or uport == "4000":
+            break
+        elif not uport.isnumeric() == True or int(uport)<1024 or int(uport) > 65535:
+            print("try again")
+        else:
+            port = uport
+            break
+    return host, int(port)
+
+
+res = checker()         
 sock = socket.socket()
-sock.bind(('', 9091))
+sock.bind(res)
 sock.listen(0)
 msg = ""
-
+print("\nSERVER UP AND RUNNING\nLIST OF CONNECTIONS:\n")
 while True:
 	conn, addr = sock.accept()
-	print(addr, '\n')
+	print(f"CONNECTION FROM\nIP = {addr[0]} PORT = {addr[1]} \n")
 	while True:
 		data = conn.recv(1024)
 		if not data:
 			print("CLIENT DISCONNECTED\n")
 			break
 		msg += data.decode()
-		conn.send(data)
+		# conn.send(data)
 		print(data.decode(),'\n')
 
 
