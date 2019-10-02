d98d509ac3b6b3f51ea8add3913530dd677802ee
Aneli <aneli.klev.a@gmail.com>
2019-10-02 17:02:34 +0300

    4

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
-import socket
+import socket, re
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', 9090))
 
+try:
+	name_port = input("Введите имя хоста: ")
+	assert re.match(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}', name_port) or name_port == "localhost","Имя порта не котируется."
+except (AssertionError, TypeError, ValueError) as e:
+	print("Имя порта по умолчанию localhost")
+	name_port = "localhost"
+
+try:
+	num_port = int(input("Введите номер порта: "))
+	assert 1024 < num_port < 65535, "Введенный порт рекомендуется не использовать или он занят."
+except (AssertionError, TypeError, ValueError) as e:
+	print("Будет введен порт по умолчанию 9091")
+	num_port = 9091
+
+sock.connect((name_port, num_port))
+print("Успешно! Можете писать сообщения")
 msg = 0 
 while msg!= "exit":
 	msg = input()
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
+try:
+	num_port = int(input("Введите номер порта:"))
+	assert 1024 < num_port < 65535, "Введенный порт рекомендуется не использовать или он занят."
+except (AssertionError, TypeError, ValueError) as e:
+	print("Будет введен порт по умолчанию 9091")
+	num_port = 9091
 
+sock.bind(('', num_port))
+sock.listen(1)
+print("Ожидайте подключения...")
 while True:
 	pass
 	conn, addr = sock.accept()
