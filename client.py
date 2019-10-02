4dc3638b629b734bca766799f2809a56f5f2cee9
Никита Асташкин <54978818+svjashennik@users.noreply.github.com>
2019-10-02 11:24:47 +0300

    начало работы дома.

--- client.py
+++ client.py
@@ -1,22 +1,47 @@
 import socket
 from time import sleep
-print("введите адрес хоста и номер порта")
-adress = input()
-port = int(input())
-if not adress:
-  adress  = '10.38.165.12'
-if not port:
-  port = 9090
+import re
+
+
+
+
+def accept(adr, port):
+    lst = [];
+    if re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', adr).group(0) == adr:
+        lst.append(adr);
+    if re.match(r'\d{1,4}', port).group(0) == port:
+        lst.append(port)
+    return lst
+
+
+
+
+
+mem = True
+while mem:
+    print("введите адрес хоста и номер порта")
+    adress = input()
+    port = input()
+    if not adress:
+        adress = '192.168.0.100'
+    if not port:
+        port = '9090'
+    check = accept(adress, port)
+    print(check)
+    if len(check) == 2:
+        mem = False
+
+port = int(port)
 sock = socket.socket()
 sock.connect((adress, port))
+print(sock.recv(1024))
+
 ex = True
 while ex:
-  msg = input()
-  if msg == "exit":
-    ex = False
-  sock.send(msg.encode())
-  data = sock.recv(1024)
-  print(data.decode())
+    msg = input()
+    if msg == "exit":
+        ex = False
+    sock.send(msg.encode())
 sock.close()  
   
 
--- server.py
+++ server.py
@@ -1,22 +1,40 @@
 import socket
 import json
+
+
+def mem(ipv):
+    with open('names.json', "a") as file:
+        memory = json.load(file)
+    if ipv not in memory:
+        return False
+    else:
+        return True
+
+
 sock = socket.socket()
-sock.bind(('10.38.165.12', 9090))
+port = 9090
+try:
+    sock.bind(('192.168.0.100', port))
+except:
+    port+=1
+    sock.bind(('192.168.0.100', port))
 sock.listen(4)
 command = ("exit")
-while True
-	conn, addr = sock.accept()
-	print(addr)
-	data = 'new'
-	while data:
-		data = conn.recv(1024)
-		data = data.decode()
-		if data not in command:
-			print(data)
-		else:
-			with open("log_serv.json", "a") as file:
-				json.dump(data, file)
-			
+while True:
+    conn, addr = sock.accept()
+    print(addr)
+    msg = 'Добрый вечер'
+    conn.send(msg.encode())
+    data = 'new'
+    while data:
+        data = conn.recv(1024)
+        data = data.decode()
+        if data not in command:
+            print(data)
+        else:
+            with open("log_serv.json", "a") as file:
+                json.dump(data, file)
+
 
 
 
