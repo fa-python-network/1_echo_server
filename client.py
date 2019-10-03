2a0fef5556f004b3876e51e38169594d566af2a0
Kirill <kirilllee1998@gmail.com>
2019-10-03 22:11:58 +0300

    kek

--- client.py
+++ client.py
@@ -8,9 +8,49 @@ sock.connect(('10.38.165.12', 9090))
 #msg = input()
 msg = "Hi!"
 sock.send(msg.encode())
+host = input('Введите имя хоста: ')
+if host == 'localhost':
+    pass
+else:
+    if any(c.isalpha() for c in host) == True:
+        print('Введено некорректное имя хоста.По умолчанию выбран локальный хост')
+        host = 'localhost'
+    else:
+        host_lst = host.split('.')
+        for i in host_lst:
+            if 0 <= int(i) <= 255:
+                pass
+            else:
+                host = 'localhost'
+                print('Введено некорректное имя хоста.По умолчанию выбран локальный хост')
 
 data = sock.recv(1024)
+try:
+    port = int(input('Введите номер порта: '))
+    if 0 <= port <= 65535:
+        pass
+    else:
+        print('Введен некорректный номер порта.Номер порта по умолчанию 9090')
+        port = 9090
+
+except ValueError:
+    print("Некорректный номер порта. Номер порта по умолчанию 9090")
+    port = 9090
 
 sock.close()
+sock.connect((host, port))
 
 print(data.decode())
+print('Напишите exit для завершения работы с сервером')
+msg = ''
+while True:
+    if msg != 'exit':
+        print('Введите сообщение:')
+        msg = input()
+        sock.send(msg.encode())
+        data = sock.recv(1024)
+    else:
+        break
+
+sock.close()
+print('Работа с сервером завершена.')
--- server.py
+++ server.py
@@ -1,20 +1,41 @@
 import socket
+import logging as l
+
+log_format = '%(levelname)s %(asctime)s - %(message)s'
+l.basicConfig(filename='server.log', format = log_format ,datefmt='%d.%m.%Y %H:%M:%S', level=l.INFO)
+
+l.info('Start logging INFO')
+
 
 sock = socket.socket()
 sock.bind(('', 9090))
+l.info ('Сервер подключился к порту: 9090')
 sock.listen(0)
 conn, addr = sock.accept()
 print(addr)
+l.info('Сервер ожидает подключения...')
 
 msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+   data = conn.recv(1024)
+   if not data:
+       break
+   msg += data.decode()
+   conn.send(data)
+    conn, addr = sock.accept()
+    l.info(f'Клиент {addr} подключился к серверу')
+    msg = ''
 
 print(msg)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.send(data)
+        l.info ('Сервер отправляет ответ клиенту')
+
 
 conn.close()
+    conn.close()
+    l.info('Соединение завершено')
