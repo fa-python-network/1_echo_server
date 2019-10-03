84efa6006b549528f16774c76c3e41eb03fe025a
root <vmolchanovag@mail.ru>
2019-10-04 00:41:27 +0300

    added full task7

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import logging
 logging.basicConfig(filename='sample.log', level=logging.INFO)
-file=open('manual.txt', 'r')
+
 
 sock = socket.socket()
 
@@ -21,22 +21,28 @@ sock.listen(1)
 logging.info('listening')
 
 while True:
+	file=open('manual.txt', 'r')
 	conn,addr=sock.accept()
 	print(addr[0])
-
+	f=0
 	for line in file:
 		l=line.strip()
-		if str(addr[1]) in l:
-			name=l.strip('-')[1]
-			print('hi,{}'.format(name))
-		else:
-			file.close()
-			ask_name='enter your name'
-			conn.send(ask_name.encode())
-			got_name=conn.recv(1024)
-			file=open('manual.txt', 'a')
-			file.write('{addr[1]} - {got_name}')
-			file.close()
+		if str(addr[0]) in l:
+			name=l.split('-')[1]
+			conn.send('hi,{}'.format(name).encode())
+			f=1
+			break
+	if f==0:
+		file=open('manual.txt', 'a+')
+		ask_name='create your name'
+		conn.send(ask_name.encode())
+		got_name=conn.recv(1024)
+		got_name.decode()
+		file=open('manual.txt', 'a')
+		file.write('\n{} - {}'.format(addr[0],got_name.decode()))
+		file.close()
+			
+
 
 	msg = ''
 	while True:
