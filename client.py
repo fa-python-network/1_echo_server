23bf33bedf153daf3f2586d080af06bdc429e72f
StepIgor <igor.stepanov@difres.ru>
2019-10-03 21:22:34 +0300

    Всё готово! Чат работает!

--- client.py
+++ client.py
@@ -1,9 +1,44 @@
-import socket
+import socket	#сам сокет
+import re	   #будем проверять айпишник
+from threading import Thread	#отдельный поток для обновления чата
 
+def check_msg():	#обновление чата
+	while True:
+		try:
+			answ = sock.recv(1024)
+			print(answ.decode())
+		except:
+			break
+
+def set_ip_and_port(user_ip='localhost',user_port=9100):	#функция для проверки и установки валидных айпи и порта
+	global port, ip	 #будем менять глобальные переменные
+	try:
+		if int(user_port) > 1024 and int(user_port) < 65000:	#проверка порта
+			port = int(user_port)
+		else:
+			port = 9100
+	except:
+		port = 9100	#дефолт
+
+	checkip = re.search('^(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])){3}$', user_ip, flags=re.IGNORECASE)	#проверка айпи регуляркой
+
+	if user_ip == 'localhost' or checkip is not None:	 #если айпи валидный
+		ip = user_ip
+	else:
+		ip = 'localhost'	#дефолт, например, если поле пусто или неверно
+	return
 
 #настройки
-port=9100
-ip='localhost'
+port=None
+ip=None
+
+
+print('Введите IP:')
+user_ip = input()
+print('Введите порт:')
+user_port = input()
+
+set_ip_and_port(user_ip=user_ip,user_port=user_port)  #пробуем применить параметры пользователя
 
 
 sock = socket.socket()  #общее для клиента и сервера
@@ -18,26 +53,25 @@ if answer.decode() == 'give_me_name':
 	newname = input()
 	sock.send(newname.encode())
 	answer = sock.recv(1024)
-	if answer.decode() == "give_me_password":
+	if answer.decode() == "give_me_password":	#если просят регистрацию
 		print("Введите новый пароль:")
 		newpassword = input()
 		sock.send(newpassword.encode())
 		print('Новый аккаунт создан. Перезапустите клиент!')
 else:
-	print("Сервер запрашивает пароль: ")
+	print("Сервер запрашивает пароль: ")	#если тебя нашли в базе и проверяют по паролю
 	password = input()
 	sock.send(password.encode())
 	answer = sock.recv(1024)
-	if answer.decode() == "bad_password":
+	if answer.decode() == "bad_password":	#если пароль неверный (флаги эти от сервера!)
 		print("Сервер отказал. Неправильный пароль!")
-	else:
+	else:	#иначе хлеб-соль, человек принят в чат!
 		print(answer.decode())
 		print('Добро пожаловать в многопользовательский чат! Поделитесь своими мыслями!')
-		while True:
+		Thread(target=check_msg).start()	#запускаем поток для обновления чата
+		while True:		#постоянный обмен сообщениями, пока клиент не напишет exit
 			msg = input()
 			sock.send(msg.encode())
-			answ = sock.recv(1024)
-			print(f'--------------\n'+answ.decode())
 			if msg == "exit":
 				print("Вы покинули чат! Всего доброго!")
 				break
@@ -47,4 +81,4 @@ print('Соединение закрыто!')
 sock.close()	#закрыть соединение
 
 print('\n\n\nКонец программы')
-wait = input()
+wait = input()	#чтобы окно сразу не закрывалось!
--- server.py
+++ server.py
@@ -1,80 +1,139 @@
-import socket
-import pickle
-import os
+import socket	#сам сокет
+import pickle	#хранение данных о клиентах
+import os		#проверка на существование файла с данными клиентов
+from threading import Thread	#потоки для работы с несколькими клиентами
 
-
-# настройки
-port = 9100
-maxclients = 1
-clientdb = 'clients-2.pkl'
-passwordsdb = 'passwords-2.pkl'
-clients = {}
-passwords = {}
-
-print('Python Socket Server ver. 0.0.1. By Igor Stepanov (PI18-2)');
-sock = socket.socket()  # общая часть для клиента и сервера
-
-sock.bind(('', port))  # занимаем 9100 порт
-print(f'Занял порт {port}')
-sock.listen(maxclients)  # начинаем слушать, ожидать единственное подключение
-print(f'Начал слушать порт {port}')
-print('Ожидаю подключений...')
-
-if os.path.getsize(clientdb) > 0:
-	with open(clientdb, 'rb') as f:
-		clients = pickle.load(f)  # получили данные клиентов
-
-if os.path.getsize(passwordsdb) > 0:
-	with open(passwordsdb, 'rb') as f:
-		passwords = pickle.load(f)  # получили пароли клиентов
-
-print('Получил данные клиентов из файла')
-print(clients.keys())
-print(passwords.keys())
-print(passwords.values())
-
-while True:  # сервер, не спи, всегда внимательно слушай!
-
-	conn, addr = sock.accept()  # принимаем от клиента сокет и его адрес
-
+def start_new_client(conn,addr):	#работа с отдельным клиентом
 	while True:
-		print(f'Подключился {addr[0]}')
-		if addr[0] in clients.keys():
-			print('Хочу проверить его пароль...')
+		try:		#ВСЕ ЛОГИ ВЕДУ ЧЕРЕЗ TRY, ТАК КАК МОЖЕТ ВЫПОЛНЯТЬСЯ НЕСКОЛЬКО ПОПЫТОК ОТКРЫТИЯ ФАЙЛА, ЛОГИ ПРЕКРАСНО ВЕДУТСЯ!
+			log_file.write(f'Подключился {addr[0]}\n')
+		except:
+			pass
+		if addr[0] in clients.keys():	#А есть ли у нас уже такой клиент в базе? - ДА!
+			try:
+				log_file.write(f'Хочу проверить его пароль...\n')
+			except:
+				pass
 			conn.send('ask_password'.encode())
 			data = conn.recv(1024)	#получить пароль
-			if passwords[addr[0]] == data.decode():
+			if passwords[addr[0]] == data.decode():	#пароль совпал?
 				answertouser = 'Привет, ' + clients[addr[0]] + '!'
 				conn.send(answertouser.encode())	#отправить имя
-				print('Отправил имя')
-				while True:
+				log_file.write(f'Отправил имя\n')
+				while True:			#отныне этот клиент с нами в чате, пока он не напишет exit!
 					msg = conn.recv(1024)
-					conn.send(f'{clients[addr[0]]}:\n{msg.decode()}'.encode())
+					for user in connected_users:
+						if user != conn:
+							user.send(f'[{clients[addr[0]]}]:{msg.decode()}'.encode())	#отправляем всем, кроме отправителя
 					if msg.decode() == "exit":
-						conn.send(f'{clients[addr[0]]} покинул чат.'.encode())
+						for user in connected_users:
+							if user != conn:
+								user.send(f'{clients[addr[0]]} покинул чат.'.encode())
+							connected_users.remove(conn)
+							try:
+								log_file.close()	#закрыть файл лога до следующего клиента
+							except:
+								pass
 						break
 
-			else:
-				print('Неправильный пароль от клиента. Сбросил соединение с ним!')
+			else:	#если пароль прислали неверный, прощаемся с клиентом
+				try:
+					log_file.write(f'Неправильный пароль от клиента. Сбросил соединение с ним!\n')
+				except:
+					pass
 				conn.send("bad_password".encode())
-		else:
+		else:	#Если клиента нет еще в базе, регистрируем!
 			conn.send('give_me_name'.encode())	#скажи мне имя
-			print('Отправил запрос на получение имени')
+			try:
+				log_file.write(f'Отправил запрос на получение имени\n')
+			except:
+				pass
 			data = conn.recv(1024)	#получить имя
-			print('Получил имя')
+			try:
+				log_file.write(f'Получил имя\n')
+			except:
+				pass
 			clients.update({addr[0]:data.decode()})
-			with open(clientdb,'wb') as f:
-				pickle.dump(clients,f)	#получили данные клиентов
+			with open(clientdb,'wb') as f:	#записали в файл
+				pickle.dump(clients,f)	#записали данные клиентов
 			conn.send('give_me_password'.encode())	#скажи мне пароль
-			print('Отправил запрос на получение пароля')
+			try:
+				log_file.write(f'Отправил запрос на получение пароля\n')
+			except:
+				pass
 			data = conn.recv(1024)	#получить пароль
 			passwords.update({addr[0]:data.decode()})
-			with open(passwordsdb,'wb') as f:
-				pickle.dump(passwords,f)	#получили данные клиентов
+			with open(passwordsdb,'wb') as f:	#записали в файл
+				pickle.dump(passwords,f)	#записали данные клиентов
 
 		break
 
-	print(clients.keys())
-	print(passwords.keys())
+	try:
+		connected_users.remove(conn)
+	except:
+		pass
 	conn.close()	#закрыть соединение с клиентом
-	print('Соединение с клиентом закрыто')
+	try:
+		log_file.write(f'Соединение с клиентом закрыто\n')
+		log_file.close()	#закрыть файл лога до следующего клиента
+	except:
+		pass
+
+# настройки
+port = 9100		#порт
+maxclients = 5	#максимальное число слушателей
+clientdb = 'clients-2.pkl'	#тут айпи - имя (словарь)
+passwordsdb = 'passwords-2.pkl'	#тут айпи - пароль (словарь)
+clients = {}	#сюда считываем айпи - имя из файла выше
+passwords = {}	#сюда считываем айпи - пароль из файла выше
+connected_users = [];	#все подключенные пользователи
+
+log_file = open('server.log','a')  #лог файл
+
+log_file.write(f'Python Socket Server ver. 0.0.1. By Igor Stepanov (PI18-2)\n');	#привествие в лог-файл
+sock = socket.socket()  # общая часть для клиента и сервера
+
+while True:	#берем первый свободный порт
+	try:	#пробуем занять порт
+		sock.bind(('',port))	#занимаем 9100 порт
+		break
+	except:
+		port+=1		#меняем порт
+
+print(f'Занял порт {port}')
+log_file.write(f'Занял порт {port}\n')
+sock.listen(maxclients)  # начинаем слушать, ожидать уже НЕ единственное подключение
+log_file.write(f'Начал слушать порт {port}\n')
+log_file.write(f'Ожидаю подключений...\n')
+
+if os.path.getsize(clientdb) > 0:	#существует ли файл
+	with open(clientdb, 'rb') as f:
+		clients = pickle.load(f)  # получили данные клиентов
+
+if os.path.getsize(passwordsdb) > 0:	#существует ли файл
+	with open(passwordsdb, 'rb') as f:
+		passwords = pickle.load(f)  # получили пароли клиентов
+
+log_file.write(f'Получил данные клиентов из файла\n')
+print(clients.keys())	#отладочная информация
+print(passwords.keys())	#отладочная информация
+print(passwords.values()) #отладочная информация
+
+print('Сервер работает!')	#отладка и статус
+
+while True:  # сервер, не спи, всегда внимательно слушай!
+
+	try:
+		log_file = open('server.log','a')	#пробуем открыть лог-файл на дописывание, чтобы каждый новый клиент фиксировался
+	except:
+		pass
+
+	conn, addr = sock.accept()  # принимаем от клиента сокет и его адрес
+
+	connected_users.append(conn)	#собираем базу подключенных пользователей для рассылки
+
+	print(connected_users)	#отладка
+
+	Thread(target=start_new_client,args=(conn,addr)).start()	#старт потока для нового клиента
+
+sock.close()
