6d0ebc3dcd5ee8bb5d9db83c18da6eb55d96f7c2
sonya <sofarogoznikova598@gmail.com>
2019-10-04 10:38:49 +0300

    z4

--- client1.py
+++ client1.py
@@ -2,14 +2,28 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', 9090))
+ggg = False
+while ggg==False:
+	try:
+		print("entered host: ")
+		host = input()
+		if host == "":
+			host = 'localhost'
+		print("entered port: ")
+		port = input()
+		if port == "":
+			port = 9090
 
-msg = input()
-while msg!='exit':
-	sock.send(msg.encode())
-	data = sock.recv(1024)
-	msg = input()
+		sock.connect((host, int(port)))
+
+		msg = input()
+		while msg!='exit':
+			sock.send(msg.encode())
+			data = sock.recv(1024)
+			msg = input()
+		ggg = True
+	except:
+		print("you entered incorrect data")
 
 print(data.decode())
 sock.close()
