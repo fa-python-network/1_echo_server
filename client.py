eb310feaa042daaa4eaba71e221be6babc886c77
Konstantin <48581717+BondarevKonstantin@users.noreply.github.com>
2019-10-04 12:19:45 +0300

    Коммит для сервера
    Ужасное исполнение клиентской и серверной части, сегодня переделаю

--- client.py
+++ client.py
@@ -1,13 +1,46 @@
 import socket
-from time import sleep
+import json
+
+with open('data.json', 'r') as f:
+    datafile = json.load(f)
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+host = input('Введите имя хоста: ')
+if host == '':
+    host = 'localhost'
+port = input('Введите порт: ')
+if port == '':
+    port = 9090
+else:
+    port = int(port)
+
+sock.connect((host, port))
+
+client = sock.recv(1024).decode()
+
+identh = sock.recv(1024).decode()
+
+if identh == '1':
+    while True:
+        password = input("Введите пароль: ")
+        if password == datafile[client]['password']:
+            print(f"Ну привет, {datafile[client]['name']}")
+            break
+        else:
+            print('Неправильный пароль, попробуй еще раз')
+else:
+    nickname = input('О, да ты зеленый, Маня! Кто по масти? ')
+    sock.send(nickname.encode())
+    pass_to_send = input('Введи новый пароль: ')
+    sock.send(pass_to_send.encode())
+
+while True:
+    msg = input()
+    sock.send(msg.encode())
+    if msg == 'exit':
+        break
+
 
 data = sock.recv(1024)
 
--- server.py
+++ server.py
@@ -1,20 +1,80 @@
 import socket
+import json
+import hashlib
+
+SALT = 'rnjrenfrenfer'.encode('utf-8')
+
+with open('data.json', 'r') as f:
+    datafile = json.load(f)
+
+
+def pass_hash(passw):
+    return hashlib.sha3_512(passw.encode('utf-8') + SALT).hexdigest()
+
+
+f = open('log.txt', 'w')
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+port = 9090
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    try:
+        if port == 65536:
+            print('Все порты заняты')
+            break
+        sock.bind(('', port))
+        break
+    except ConnectionResetError:
+        port += 1
 
-print(msg)
+user = ''
+print('Сервер запущен', file=f)
+print(f'Прослушиваемый порт: {port}', file=f)
+sock.listen(0)
+print('Сервер прослушивается', file=f)
 
-conn.close()
+while True:
+    conn, addr = sock.accept()
+    msg = ''
+
+    client = addr[0]
+    conn.send(addr[0].encode())
+
+    if client in datafile.keys():
+        conn.send('1'.encode())
+        print('Пользователь найден в базе', file=f)
+        nick = datafile[client]["name"]
+        password = datafile[client]["password"]
+    else:
+        conn.send('0'.encode())
+        print('Пользователь не найден в базе, запись нового', file=f)
+        user = 'new'
+        nick = conn.recv(1024).decode()
+        password = pass_hash(conn.recv(1024).decode())
+
+    try:
+        while True:
+            data = conn.recv(1024)
+            print('Принят поток данных', file=f)
+            if not data:
+                break
+            msg += data.decode() + ' '
+            conn.send(data)
+            print('Данные обработаны и отправлены клиенту', file=f)
+    except ConnectionAbortedError:
+        datafile[client] = {}
+        datafile[client]['ip'] = addr[0]
+        datafile[client]['port'] = port
+        datafile[client]['name'] = nick
+        datafile[client]['password'] = password
+
+        with open('data.json', 'w') as file:
+            json.dump(datafile, file)
+        conn.close()
+        print('Связь с клиентом оборвана', file=f)
+        command = input('Если вы хотите остановить сервер введите: "stop"\n ')
+        if command == 'stop':
+            sock.close()
+            print('Сервер остановлен', file=f)
+            f.close()
+            break
