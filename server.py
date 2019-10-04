ef0bf6a41ada5811bd56cfb89713e960b991167e
Yuriy <superbober68@mail.ru>
2019-10-04 12:35:32 +0300

    task 3 v.01

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import socket
 
+port = 9090
+clients = 1
+
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+sock.listen(clients)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
 
-print(msg)
-
-conn.close()
+    conn, addr = sock.accept()
+    
+    while True:
+        
+        data = conn.recv(1024)
+        if not data:
+            break
+            
+      	conn.send(data)
+        print("Answer sent")
+        
+        conn.close()
+        print("Connection is cut off")
