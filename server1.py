89121eed1d996b6467d532417bf721321cc75b06
sonya <sofarogoznikova598@gmail.com>
2019-10-04 10:20:03 +0300

    z3

--- server1.py
+++ server1.py
@@ -3,16 +3,22 @@ import socket
 sock = socket.socket()
 sock.bind(('',9090))
 sock.listen(1)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ""
+
 
 while True:
-	data = conn.recv(10)
-	if not data:
-		break
-	msg += data.decode()
+
+	msg = ""
+	conn,addr = sock.accept()
+	print(addr)
+
+
+	while True:
+		
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg += data.decode()
 
 conn.close()
 sock.close()
