05be7174bd53912278634800b95290643f06d366
Vlad <vlad07020@mail.ru>
2019-10-04 21:50:04 +0300

    zadanie_8_+

--- client1.py
+++ client1.py
@@ -6,19 +6,25 @@ server_standart = 'localhost'
 
 
 server = input("Введите номер сервера: ")
-server1 = server.split(".", 4)
-for el in server1:
-	el = int(el)
-	if 0<=el<=255:
-		pass
-	else:
-		server = server_standart
+if server == "":
+	server = server_standart
+else:
+	server1 = server.split(".", 4)
+	for el in server1:
+		el = int(el)
+		if 0<=el<=255:
+			pass
+		else:
+			server = server_standart
 
 port = input("Введите номер порта: ")
-if 1024<=int(port)<=65535:
-	pass
-else:
+if port == "":
 	port = port_standart
+else:
+	if 1024<=int(port)<=65535:
+		pass
+	else:
+		port = port_standart
 
 
 sock.connect((server, int(port)))
