d24b454aea4cb604a6a186365c9ccb9b3f9a2c6f
bub <kruchininayana26@gmail.com>
2019-10-04 18:29:55 +0300

    ya ustal

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 import socket
-from time import sleep
+
+
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+print('Клиент запущен')
+port = 8564
+sock.bind(('', port)) 
+print('Подключено к серверу')
+msg = ''
+while msg != 'exit':
+    msg = input()
+    print('Отправляю сообщение {}'.format(msg))
+    sock.send(msg.encode())
+    print('Получаю ответ сервера')
+    received_msg = sock.recv(1024)
+    print(received_msg.decode())
 sock.close()
 
-print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,41 @@
 import socket
+from re import match
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+print('Запуск сервера')
+host = 'localhost'
+port = 135  # 9090
+host_ = input('Host: ')
+port_ = input('Port:')
+host_regex = r'[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]'
+if host_ and match(host_regex, host_):
+    host = host_
+print(f'current host:{host}')
+if port_.isdigit() and 1024 <= int(port_) <= 65535:
+    port = int(port_)
 
-msg = ''
+try:
+    sock.bind(('', port))
+except OSError:
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+print('Сервер слушает порт {}'.format(port))
+print(f'current port: {port}')
+sock.listen(5)
 
-print(msg)
+try:
+    while 1:
+        conn, addr = sock.accept()
+        print('Подключен клиент {}:{}'.format(*addr))
+        while 1:
+            received_msg = conn.recv(1024).decode()
+            conn.send(received_msg.encode())
+            if not received_msg:
+                print('Разрыв соединения')
+                conn.close()
+                break
 
-conn.close()
+finally:
+    print('Сервер завершает работу')
+    sock.close()
