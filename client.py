d6223b4ea8a312363cde513754492fcd73d8090e
Anton <anton.reient@gmail.com>
2019-10-04 19:51:03 +0300

    dd

--- client.py
+++ client.py
@@ -1,17 +1,27 @@
 import socket
+import hashlib
+import re
+from os import listdir
 
 
 def settings():
-	"""
-	Настройка подключения к серверу
-	"""
-	host_prompt = input('Type the HOST if needed: ')
-	port_prompt = input('Type the PORT if needed: ')
+    host_prompt = input('Type the HOST if needed: ')
+    port_prompt = input('Type the PORT if needed: ')
 
-	global HOST
-	global PORT
-	HOST = '127.0.0.1' if not host_prompt else host_prompt
-	PORT = 9014 if not port_prompt else int(port_prompt)
+    global HOST
+    global PORT
+    HOST = '127.0.0.1' if not host_prompt else host_prompt
+    PORT = 9014 if not port_prompt else int(port_prompt)
+
+
+def modify_password(password: str) -> str:
+    hash_object = hashlib.md5(password.encode())
+    return hash_object.hexdigest()
+
+
+def create_cookie(cookie: str) -> None:
+    with open('cookie', 'w') as f:
+        f.write(cookie)
 
 
 if __name__ == '__main__':
@@ -19,10 +29,21 @@ if __name__ == '__main__':
 
     with socket.socket() as s:
         s.connect((HOST, PORT))
-        s.sendall(b'Hi, dingus')
+
+        if 'cookie' in listdir():
+            with open('cookie', 'r') as f:
+                s.sendall(f.readline().encode())
 
         while True:
             data = s.recv(1024)
-            s.sendall(bytes(input(), 'utf-8'))
+            print(data.decode())
 
-    print(f'Received {data}')
+            reply = input()
+
+            if re.match(r'(\w+);(\w+)', reply):
+                re_reply = re.search(r'(\w+);(\w+)', reply)
+                user_name, password = re_reply.group(1), modify_password(re_reply.group(2))
+                reply = f'{user_name};{password}'
+                create_cookie(modify_password(re_reply.group(2)))
+            
+            s.sendall(bytes(reply, 'utf-8'))
--- server.py
+++ server.py
@@ -1,49 +1,82 @@
 import socket
 import time
+import json
+from os import listdir
+import hashlib
+import re
 
 
-def settings():
-	"""
-	Настройка сервера
-	"""
+def settings() -> None:
 	host_prompt = input('Type the HOST if needed: ')
 	port_prompt = input('Type the PORT if needed: ')
 
 	global HOST
 	global PORT
 	HOST = '127.0.0.1' if not host_prompt else host_prompt
-	PORT = 9014 if not port_prompt else int(port_prompt)
+	PORT = 0 if not port_prompt else int(port_prompt)
 
 
-def write_to_file(msg: str, filename='log.txt'):
-	"""
-	Записівает сообщения в файл
-	"""
+def write_to_file(msg: str, filename='log') -> None:
 	with open(filename, 'a') as f:
 		f.write(f'-----------------------\ntime: {time.time()}\nmsg: {msg}\n')
+
+
+def register_new_client(ip_address: str, user_name: str, password: str) -> None:
+	user_db[ip_address] = {'user_name': user_name, 'password': password}
+
+	with open('user_db', 'w') as f:
+		json.dump(user_db, f)
+	
+	write_to_file('----Successfully added a new client----')
 
 
 if __name__ == '__main__':
 	settings()
 
+	if 'user_db' not in listdir():
+		with open('user_db', 'w') as f:
+			f.write('{}')
+		print('----No user database existed; created a new one----')
+	
+	with open('user_db', 'r') as f:
+		user_db = json.load(f)
+		print('----Successfully loaded the user database----')
+
 	with socket.socket() as s:
 		s.bind((HOST, PORT))
+		print(f'The port is {s.getsockname()[1]}')
 		s.listen()
 
 		while True:
 			conn, addr = s.accept()
 
-			write_to_file('Accepted a new request')
+			write_to_file('----Accepted a new request----')
 
 			with conn:
-				write_to_file(f'Connected by {addr}')
+				write_to_file(f'----Connected by {addr}----')
 
-				while True:
+				if addr[0] not in user_db:
+					conn.sendall(b'You\'re not in the records. Send us your new user name and password as follows:\nUSER_NAME;PASSWORD')
+				else:
+					conn.sendall(b'Enter the password, please')
 					data = conn.recv(1024)
+					if data.decode() == user_db[addr[0]]['password']:
+						conn.sendall(f'Welcome back, {user_db[addr[0]]["user_name"]}'.encode())
+				
+				while True:
+					
+					data = conn.recv(1024)
+
+					if addr[0] not in user_db:
+						if re.match(r'(\w+);(\w+)', data.decode()):
+							reply = re.search(r'(\w+);(\w+)', data.decode())
+							user_name, password = reply.group(1), reply.group(2)
+							register_new_client(addr[0], user_name, password)
+						else:
+							continue
+
 					write_to_file(data.decode())
 
 					if not data or data == b'exit':
-						write_to_file('Stopped serving a user')
+						write_to_file('----Stopped serving a user----')
 						break
-
-					conn.sendall(data)
