83f221f0e5e4f02bb7e2ba67b9d0c04d6a855ccb
LiliyaRachkova <lili.rachkova@ya.ru>
2019-10-04 19:54:49 +0300

    n8

--- client.py
+++ client.py
@@ -3,42 +3,44 @@ from time import sleep
 
 sock = socket.socket()
 k = False
-while k==False:
-	try:
-		print("host:")
-		host = input()
-		if host == "":
-			host = 'localhost'
+while k == False:
+    try:
+        print("host:")
+        host = input()
+        if host == "":
+            host = 'localhost'
 
-		print("port:")
-		port = input()
-		if port == "":
-			port = 9090
-		#
-		sock.connect((host, int(port)))
-		c = sock.recv(1024)
-		#new user
-		if c == "What is your name?":
-			msg = input()
-			sock.send(msg.encode())
-			#new password
-			c1 = sock.recv(1024)
-			c1 = input()
-			sock.send(c1.encode())
-			c = sock.recv(1024)
-		print(c.decode())
-		msg = input()
+        print("port:")
+        port = input()
 
-		
-		while msg!='exit':
-			sock.send(msg.encode())
-			msg = input()
-		k=True
-	except KeyboardInterrupt:
-		break
-	except:
-		print("wrong host or port")
-	data = sock.recv(1024)
+        if port == "":
+            port = 9090
+        #
+        sock.connect((host, int(port)))
+        c = sock.recv(1024)
+        # создаем нового пользователя, если его нет в файле
+        if c.decode() == "What is your name?":
+            print(c.decode())
+            msg = input()
+            sock.send(msg.encode())
+            # пароль
+            psw_msg = sock.recv(1024)
+            print(psw_msg.decode())
+            psw = input()
+            sock.send(psw.encode())
+            c = sock.recv(1024)
+        print(c.decode())
+        msg = input()
 
-#print(data.decode())
+        while msg != 'exit':
+            sock.send(msg.encode())
+            msg = input()
+        k = True
+    except KeyboardInterrupt:
+        break
+    except:
+        print("wrong host or port")
+    data = sock.recv(1024)
+
+# print(data.decode())
 sock.close()
--- server.py
+++ server.py
@@ -3,10 +3,8 @@ import json
 
 
 def write_into_json(dct):
-	with open('users.json', 'w') as f:
-		json.dump(dct, f)
-
-
+    with open('users.json', 'w') as f:
+        json.dump(dct, f)
 
 
 users = {}
@@ -14,51 +12,48 @@ port = 9090
 file = open("log.txt", "a")
 sock = socket.socket()
 
-#open file to take dict from it
-with open('users.json','r') as f:
-	users = json.load(f)
+# open file to take dict from it
+with open('users.json', 'r') as f:
+    users = json.load(f)
 
 try:
-	sock.bind(('', port))
-except OSError: 
-	sock.bind(('',0))
-	port = sock.getsockname()[1]
-	print(f"use port {port}")
+    sock.bind(('', port))
+except OSError:
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    print(f"use port {port}")
 file.write(f'server uses port {port}\n')
 
 sock.listen()
 file.write(f'server starts listen client\n')
 
-
 while True:
-	conn, addr = sock.accept()
-	print(addr[0])
-	try:
-		c = users[addr[0]]
-	except KeyError:
-		conn.send(b"What is your name?")
-		users[addr[0]] = []
-		users[addr[0]].append((conn.recv(1024)).decode())
+    conn, addr = sock.accept()
+    print(addr[0])
+    try:
+        c = users[addr[0]]
+    except KeyError:
+        conn.send(b"What is your name?")
+        users[addr[0]] = []
+        users[addr[0]].append(conn.recv(1024).decode())
 
-		conn.send(b"Write new password")
-		users[addr[0]].append((conn.recv(1024)).decode())
+        conn.send(b"Write new password")
+        users[addr[0]].append(conn.recv(1024).decode())
 
+    msg_to_client = f'Hi,{users[addr[0]][0]}!'
+    conn.send(msg_to_client.encode())
+    write_into_json(users)
 
-	msg_to_client = f'Hi,{users[addr[0]][0]}!'
-	conn.send(msg_to_client.encode())
-	write_into_json(users)	
+    msg = ""
 
-	msg = ""
-
-	while True:
-		file.write(f'server receives datd from client {addr}\n')
-		data = conn.recv(1024)
-		if not data:
-			break
-		print(data.decode())
-		conn.send(data)
-		file.write(f'server responds to a client\n')
-
+    while True:
+        file.write(f'server receives datd from client {addr}\n')
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(data.decode())
+        conn.send(data)
+        file.write(f'server responds to a client\n')
 
 file.close()
 conn.close()
