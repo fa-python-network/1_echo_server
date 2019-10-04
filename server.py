2b44ec6255ad1a27dbe488867aa80ece81ae481b
Sangadzhi <zaiasg@mail.ru>
2019-10-04 21:28:33 +0300

    5 пункт

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
 import socket
+import logging as l
 
+log_format = '%(levelname)s %(asctime)s - %(message)s'
+l.basicConfig(filename='server.log', format = log_format ,datefmt='%d.%m.%Y %H:%M:%S', level=l.INFO)
+
+l.info('Start logging Info')
 sock = socket.socket()
 sock.bind(('', 9090))
+l.info ('Сервер подключился к порту: 9090')
 sock.listen(0)
+l.info('Сервер ожидает подключения...')
 
 while True:
         conn, addr = sock.accept()
-        print('Клиент {addr} подключился к серверу')
+        l.info('Клиент {addr} подключился к серверу')
         msg = ''
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            msg +=" " + data.decode()
             conn.send(data)
 
         conn.close()
-        print('Соединение завершено')
+        l.info('Соединение завершено')
