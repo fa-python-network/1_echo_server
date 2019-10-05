cc48163b052cb301b5792095ebe4ea361e674e96
Artemiy Saltsin <russhun638@gmail.com>
2019-10-05 21:52:36 +0300

    task 4/11

--- client.py
+++ client.py
@@ -2,7 +2,28 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.connect(('127.0.0.1', 9090))
+default_pair = ('127.0.0.1', 9090)
+
+ip = input("Введите IP сервера: ")
+while True:
+    try:
+        port = int(input("Введите порт сервера: "))
+        if port > 65535 or port < 0:
+            print("Порт должен быть числом в диапазоне 0-65535")
+        else:
+            break
+    except TypeError:
+        print("Порт должен быть числом в диапазоне 0-65535")
+
+try:
+    sock.connect((ip, port))
+except OSError:
+    print(f"Что-то пошло не так, подключаемся по стандартной паре {default_pair}")
+    try:
+        sock.connect(default_pair)
+    except ConnectionError:
+        print("Похоже, сервер забыли поднять =(")
+        exit(0)
 
 while True:
     msg = input()
--- server.py
+++ server.py
@@ -1,21 +1,42 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
+default_port = 9090
+while True:
+    try:
+        port = int(input("Введите порт на котором хотите меня поднять: "))
+        if port > 65535 or port < 0:
+            print("Порт должен быть числом в диапазоне 0-65535")
+        else:
+            break
+    except TypeError:
+        print("Порт должен быть числом в диапазоне 0-65535")
+
+try:
+    sock.bind(('', port))
+except ConnectionError:
+    print("Похоже, к этому порту нельзя причалиться =( пробую стандартный . . .")
+    try:
+        sock.bind(('', default_port))
+    except Exception:
+        print("Случилось что-то сверхъестественное . . .")
+        exit(-1)
+
 sock.listen(0)
 
+
 while True:
-	conn, addr = sock.accept()
-	print(addr)
+    conn, addr = sock.accept()
+    print(addr)
 
-	msg = ''
+    msg = ''
 
-	while True:
-		data = conn.recv(1024)
-		if not data:
-			break
-		msg += data.decode()
-		conn.send(data)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg += data.decode()
+        conn.send(data)
 
-	print(msg)
-	conn.close()
+    print(msg)
+    conn.close()
