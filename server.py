f78fe11051de9e89a9bf7e4224becb4bc5e9f719
Ваше Имя <kajit.konstantin@yandex.ru>
2019-10-05 17:59:50 +0300

    HotRefactoring

--- server.py
+++ server.py
@@ -8,88 +8,106 @@ def close_server():
 	sock.close()
 	sys.exit()
 
-try: 
-	sock = socket.socket()
-except KeyboardInterrupt:
-	close_server()
+sock = socket.socket()
+def main():
+    global sock
+
+    #Поиск свободного порта
+    port = 9090
+    flag = 1
+    while flag:
+        try:
+            sock.bind(('', port))
+        except socket.error as e:
+            if e.errno == errno.EADDRINUSE:
+                port+=1
+        else:
+            flag = 0
+    print(port)
 
 
-#Поиск свободного порта
-port = 9090
-flag = 1
-while flag:
+    #Считывание всех зареганых клиентов
     try:
-        sock.bind(('', port))
-    except socket.error as e:
-        if e.errno == errno.EADDRINUSE:
-            port+=1
-    else:
-        flag = 0
-print(port)
-
-
-#Считывание всех зареганых клиентов
-try:
-    with open ('users.txt', 'r') as users:
-        d = eval(users.read())
-except FileNotFoundError:
-    with open ('users.txt', 'w') as users:
-        d={}
-except SyntaxError:    
-        d={}
+        with open ('users.txt', 'r') as users:
+            d = eval(users.read())
+    except FileNotFoundError:
+        with open ('users.txt', 'w') as users:
+            d={}
+    except SyntaxError:    
+            d={}
 
 
 
-with open ('log.txt', 'a') as file:
-		print('Запуск сервера', file=file)
-try:
-	sock.listen(1)
-	with open ('log.txt', 'a') as file:
-		print('Начало прослушивания порта', file=file)
-except KeyboardInterrupt:
-	close_server()
+    with open ('log.txt', 'a') as file:
+		    print('Запуск сервера', file=file)
+    sock.listen(1)
 
-while True:
+    with open ('log.txt', 'a') as file:
+	    print('Начало прослушивания порта', file=file)
 
-    try:
+
+    while True:
+
+
         conn, addr = sock.accept()
         with open ('log.txt', 'a') as file:
-            print('Подключение клиента', file=file)
-    except KeyboardInterrupt:
-        close_server()			
-    
-#Считывание имени клиеента
-    if addr[0] not in d.keys():
-        flag = str(1)
-        conn.send(flag.encode())
-        conn.send("Введите своё имя:".encode())
-        name = conn.recv(1024).decode()
-        d[addr[0]] = d.get(addr[0], name)
-        with open ('users.txt', 'w') as users:
-            print(d, file=users)
-    else:
-        flag = str(0)
-        conn.send(flag.encode())
-        conn.recv(1024)        
-    msg = 'Hello ' + d[addr[0]]
-    conn.send(msg.encode())
-    
-    while True:
-        try:
+            print('Подключение клиента', file=file)		
+        
+    #Считывание имени клиеента
+        if addr[0] not in d.keys():
+            flag = str(1)
+            conn.send(flag.encode())
+            conn.send("Введите своё имя:".encode())
+            name = conn.recv(1024).decode()
+            d[addr[0]] = d.get(addr[0], name)
+            with open ('users.txt', 'w') as users:
+                print(d, file=users)
+        else:
+            flag = str(0)
+            conn.send(flag.encode())
+            conn.recv(1024)        
+        msg = 'Hello ' + d[addr[0]]
+        conn.send(msg.encode())
+        
+        while True:
             data = conn.recv(1024).decode()
             with open ('log.txt', 'a') as file:
                 print('Приём данных от клиента', file=file)
-        except KeyboardInterrupt:
-            close_server()
-        if not data:
-            conn.close()
-            break
-        try:
+            if not data:
+                conn.close()
+                break
+            
             conn.send(data.upper().encode())
             with open ('log.txt', 'a') as file:
                 print('Отправка данных клиенту', file=file)
-        except KeyboardInterrupt:
-            close_server()
 
-    with open ('log.txt', 'a') as file:
-        print('Отключение клиента', file=file)
+
+        with open ('log.txt', 'a') as file:
+            print('Отключение клиента', file=file)
+
+
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    close_server()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
