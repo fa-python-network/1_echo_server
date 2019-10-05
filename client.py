15a9b743026bd3b0b86c805a9f1638e3c74802a3
Artemiy Saltsin <russhun638@gmail.com>
2019-10-05 21:27:30 +0300

    task 3/11

--- client.py
+++ client.py
@@ -2,15 +2,15 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('127.0.0.1', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
+while True:
+    msg = input()
+    if msg == "exit":
+        break
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.decode())
 
 sock.close()
 
-print(data.decode())
