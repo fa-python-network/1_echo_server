b52006a93bf037c4756f0874cf8b74a4ea7bc6d6
Gamid <Gamid@mail.ru>
2019-10-06 20:32:28 +0300

    задание 6+исправление 4

--- client.py
+++ client.py
@@ -6,31 +6,37 @@ sock.setblocking(1)
 host = 'localhost'
 port = 9090
 
+try:
+	print('выбран хост: ', host, 'изменить? Y/N')
+	f = input()
+	if f.upper() == 'Y':
+		q = 0
+		while q==0:
+			host = input('введите имя хоста: ')
+			for el in host.split('.'):
+				if int(el)>= 0 and int(el)<=255:
+					q = 1
+				else:
+					print('некорректные данные, повторите ввод')
+					break
+except:
+	port = 9090
+	print('некорректные данные, выбран хост "localhost"')
 
-print('выбран хост: ', host, 'изменить? Y/N')
-f = input()
-if f.upper() == 'Y':
-	q = 0
-	while q==0:
-		host = input('введите имя хоста: ')
-		for el in host.split('.'):
-			if int(el)>= 0 and int(el)<=255:
+try:
+	print('выбран порт:', port, 'изменить? Y/N')
+	f = input()
+	if f.upper() == 'Y':
+		q = 0
+		while q==0:
+			port = int(input('введите номер порта: '))
+			if port>=1024 and port<=65535:
 				q = 1
 			else:
 				print('некорректные данные, повторите ввод')
-				break
-
-
-print('выбран порт:', port, 'изменить? Y/N')
-f = input()
-if f.upper() == 'Y':
-	q = 0
-	while q==0:
-		port = int(input('введите номер порта: '))
-		if port>=1024 and port<=65535:
-			q = 1
-		else:
-			print('некорректные данные, повторите ввод')
+except:
+    port = 9090
+    print('некорректные данные, выбран порт 9090')
 
 
 sock.connect((host,port))
--- server.py
+++ server.py
@@ -1,20 +1,33 @@
 import socket
-
+import random
 sock = socket.socket()
 
 port = 9090
-print('выбран порт:', port, 'изменить? Y/N')
-f = input()
-if f.upper() == 'Y':
-	q = 0
-	while q==0:
-		port = int(input('введите номер порта: '))
-		if port>=1024 and port<=65535:
-			q = 1
-		else:
-			print('некорректные данные, повторите ввод')
+try:
+    print('выбран порт:', port, 'изменить? Y/N')
+    f = input()
+    if f.upper() == 'Y':
+	    q = 0
+	    while q==0:
+		    port = int(input('введите номер порта: '))
+		    if port>=1024 and port<=65535:
+			    q = 1
+		    else:
+			    print('некорректные данные, повторите ввод')
 
-sock.bind(('', port))
+except:
+    port = 9090
+    print('некорректные данные')
+
+f=0
+while f!=1:
+    try:
+        sock.bind(('', port))
+        f=1
+    except:
+        port=random.randint(1024,65535)
+print('слушаю порт: ', port)
+
 sock.listen(0)
 conn, addr = sock.accept()
 print(addr)
