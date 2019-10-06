516d3f998e1af0f4d48da590e9639a4c34b2871a
Daria <dalidass@mail.ru>
2019-10-06 10:56:56 +0300

    Задания 2-4

--- my_client.py
+++ my_client.py
@@ -1,35 +1,35 @@
 import socket
-from time import sleep
-
-while True:
-      host_name = input('Введите имя хоста:(например localhost) ')
-      if host_name == 'localhost':
-      		break
-      else:
-	        print('Ошибка')
-while True:
-      port = int(input('Введите порт: '))
-      if 1024 <= port <= 65525:
-         break
-      else:
-         print('Неправильный порт ')
 sock=socket.socket()
+while True:
+       host_name = input('Введите имя хоста или его адрес:(например localhost) ')
+       if host_name == 'localhost':
+              hosst_name='127.0.0.1'
+              break
+       host_ad=host_name.split('.')
+       if (0<int(host_ad[0])<255) and (0<int(host_ad[1])<255) and (0<int(host_ad[2])<255) and (0<int(host_ad[3]<255)):
+              break
+       else:
+              print('Неверный формат адреса')
+while True:
+       port = int(input('Введите порт: '))
+       if 1024 <= int(port) <= 65525:
+              break
+       else:
+              print('Неправильный номер порта ')
+              break
 sock.setblocking(1)
 print(f'Идет подключение к хосту {host_name} и порту {port} ')
 print('Идет подлючение к порту..')
 sock.connect((host_name,port)) #Подключаемся к порту
 
-#msg=input()
-#msg="Hello, server!"
 while True:
-      msg = input('Для окончания работы с сервером введите exit ')
-      if msg == "exit":
-              sock.close()
-              break
-      sock.send(msg.encode())
-
-data = sock.recv(1024)
+        msg = input('Введите свое сообщение, если вы хотите окончить работу, то вводите exit: ')
+        sock.send(msg.encode())
+        if msg == 'exit':
+            break
+print('Сообщение отправлено ')
 sock.close()
-print(data.decode())
+
+
 
 
--- my_server.py
+++ my_server.py
@@ -1,28 +1,30 @@
 import socket
-while True:
-      port=int(input('Введите порт :'))
-      if 1024<=port<=65525:
-              break
-      else:
-              print('Ошибка.Попробуйте еще раз ввести порт')
-
 sock = socket.socket()
-sock.bind(("", port))
-sock.listen(1)
 while True:
-      conn,addr = sock.accept() #принимаем сокет и адрес
-      print('Вывожу адрес..')
-      print(addr)
-
-msg = ''
-
-while True:
-      data = conn.recv(1024)
-      if not data:
+       host_name = input('Введите имя хоста или его адрес:(например localhost) ')
+       if host_name == 'localhost':
+              hosst_name='127.0.0.1'
               break
-msg = data.decode()
-conn.send(data)
+       host_ad=host_name.split('.')
+       if (0<int(host_ad[0])<255) and (0<int(host_ad[1])<255) and (0<int(host_ad[2])<255) and (0<int(host_ad[3]<255)):
+              break
+       else:
+              print('Неверный формат адреса')
+while True:
+       port=int(input('Введите порт :'))
+       if 1024<=port<=65525:
+              break
+       else:
+              print('Ошибка.Попробуйте еще раз ввести порт')
+sock.bind(('', port))             
+print(f'Слушаю ваш порт {port}')
+sock.listen(1)
+conn, addr = sock.accept()
+print('Подключаюсь и вывожу адрес..',addr)
+while True:
+        msg = conn.recv(1024)
+        print(msg.decode())
+        if msg.decode() == 'exit':
+            break
 conn.close()
-
-
 print('Соединение с клиентом закрыто')
