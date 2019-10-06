26d2bc92aaf9469d73955acc7908e8bd9fb94b78
Shalyapin2008 <55088406+Shalyapin2008@users.noreply.github.com>
2019-10-07 00:26:11 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,16 +1,43 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+print("***Введите IP или L для поключения к локальному серверу***")
+Ip=input()
+if Ip=="L":
+	Ip='localhost'
+else:
+	pass
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+print("***Введите порт или L для поключения к локальному серверу***")
+port=int(input())
+if port=="L":
+	port=9090
+else:
+	pass
+print("ip=",Ip, "port=",port)
+try:
+	sock.connect((Ip, port))
+	print("Подключение прошло успешно, введите сообщение")
+	msg=""
+	print("отправка данных")
+	try:
+		msg=input()
+		while msg!="stop":
+			sock.send(msg.encode())
+			data = sock.recv(1024)			
+			msg=input()
+		 
+	except:
+		print("данные не отправлены")
+		sock.close
 
-data = sock.recv(1024)
+	sock.close()
+	data = sock.recv(1024)
 
-sock.close()
+	print(data)
+except:
+	print("подключение оборвалось")
+	sock.close()
+	
 
-print(data.decode())
+
--- server.py
+++ server.py
@@ -1,20 +1,66 @@
 import socket
+import datetime
+
+
+
+logs=[]
+
+
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+#цикл while отвечает за bind свободного порта
+a=0
+sckt=9090
+while  a!=1 and sckt<15000:
+	try:
+		sock.bind(('', sckt))
+		a+=1
+		print("***сервер зарезервировал порт N"+str(sckt))
+		logs.append(str(datetime.datetime.now())+" зарезервировван порт N")
+	except:
+		sckt+=1
+		print(sckt)
+	
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+print("***Сервер запущен***")
+logs.append(str(datetime.datetime.now())+" запущен сервер")
 
-print(msg)
+sock.listen(1)
 
+
+
+command=''
+while command!="stop":
+	
+	sock.listen(1)
+	print("***Ожидание подключения***")
+	conn, addr = sock.accept()
+	logs.append(str(datetime.datetime.now())+" Установлено соединение с "+addr[0])
+	print ('***Установлено соединение с:', addr,'***')
+
+	while True:
+	    data = conn.recv(1024)
+	    msg=''
+	    if not data:
+	        break
+	    msg+=data.decode() 
+	    conn.send(data.upper())
+	    print(msg)
+	logs.append(str(datetime.datetime.now())+" пользователь "+ addr[0]+" отключился")
+	print("***Пользователь "+addr[0]+" отключился***")
+	print("***Продолжить или отключить сервер?***")
+	command=input()
+logs.append(str(datetime.datetime.now())+" сервер завершает работу")
+logs.append("---"*20)
+
+try:
+	f=open('logs.txt','a')
+	for i in logs:
+		f.write(i+'\n')
+except:
+	f=open('logs.txt','w')
+	for i in logs:
+		f.write(i+'\n')
+f.close()
 conn.close()
