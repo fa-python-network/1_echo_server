42990467b22d51d42c37153c4e1dd40184df37fd
Максим <maks.sochi99@mail.ru>
2019-10-07 22:14:03 +0300

    3 пункт на месте

--- server.py
+++ server.py
@@ -9,18 +9,19 @@ sock.bind((HOST, PORT))
 print('Сервер запускается, немного терпения!')
 print('Начинаем слушать порт...')
 
-sock.listen(1)
-conn, addr = sock.accept()
-print('Готов к обмену данными с клиентом')
-
-msg = ''
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	else:
-		msg += data.decode()
-		conn.send(data.decode().upper().encode())
+	sock.listen(1)
+	conn, addr = sock.accept()
+	print('Готов к обмену данными с клиентом')
 
-print(msg, sep=' ') #хотел через sep сделать разделитель, но почему то не пошло!
-conn.close()
+	msg = ''
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		else:
+			msg += data.decode()
+			conn.send(data.decode().upper().encode())
+
+	print(msg, sep=' ') #хотел через sep сделать разделитель, но почему то не пошло!
+	conn.close()
