ac114f6c343ad3d1eabc50377b70da245afda838
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-10-07 21:56:44 +0300

    8-11 пункт

--- client.py
+++ client.py
@@ -1,26 +1,88 @@
 import socket
+from threading import Thread
 from time import sleep
+import sys
+import pickle
+from getpass import getpass
 
-def sendPassword(sock):
-    txt = sock.recv(1024).decode()
-    passwd = input(txt)
-    sock.send(passwd.encode())
+class Client: 
+
+    '''
+    init status - отслеживает статус приложения
+    Возможные состояния: auth, passwd, finish, success 
+    '''
+
+    def __init__(self, sock, port, status = None):
+        self.sock = sock
+        self.port = port
+        self.status = status
+        self.name = input("Напишите ваше имя:")
+        self.start()  
+
+
+    def start(self):
+        Thread(target=self.recv).start()
+        '''
+        Проверяю какой статус приложения и что от него 
+        вообще хочет server и что он ему прислал
+        '''
+        while self.status != 'finish':
+            if self.status:
+               if self.status == "auth":
+                   self.auth()
+               elif self.status == "passwd":
+                   self.sendPasswd()
+               elif self.status == "success":
+                   self.success()
+               else:
+                   msg = input()
+                   if msg == "exit": 
+                       self.status = "finish"
+                       break
+                   sendM = pickle.dumps(["message",f'{self.name} > {msg}'])
+                   self.sock.send(sendM)
+
+        self.sock.close()
+
+    '''
+
+    отправка пароля
+
+    '''
+    def sendPasswd(self):
+        passwd = getpass(self.data)
+        self.sock.send(passwd.encode())
+        sleep(1.5)
+
+
+    def auth(self):
+        passwd = getpass(self.data)
+        self.sock.send(passwd.encode())
+        sleep(1.5)
+
+
+    def success(self):
+        print(self.data)
+        self.status = "ready"
+
+
+    def recv(self):
+        while True:
+            try:
+                self.data = self.sock.recv(1024)
+                if not self.data: sys.exit(0)
+                status = pickle.loads(self.data)[0]
+                self.status = status
+                if self.status == "message":
+                    print(pickle.loads(self.data)[1])
+                else:
+                    self.data = pickle.loads(self.data)[1]
+            except OSError:
+                break
+
 sock = socket.socket()
 sock.setblocking(True)
 port =  int(input("Порт:"))
 port = port if (port >= 0 and port <= 65535)  else  9090
-sock.connect((input('Имя хоста:'), port))
-txt = sock.recv(1024).decode()
-if "знаю" in txt:
-    name = input(txt)
-    sock.send(name.encode())
-    sendPassword(sock)
-elif "пароль" in txt:
-    passwd = input(txt)
-    sock.send(passwd.encode())
-msg = input()
-while msg != 'exit':
-    sock.send(msg.encode())
-    msg = input()
-
-sock.close()
+sock.connect((input('Имя хоста:'), port))           
+l = Client(sock, port)
--- server.py
+++ server.py
@@ -4,66 +4,60 @@ from contextlib import closing
 import os
 import hashlib
 import json
+from sc import SocketMethods
+import pickle
+from threading import Thread
+import sys
+
+
 class Server():
-	def __init__(self, log = "file.log", users = "users.json"):
+
+	def __init__(self,log = "file.log", users = "users.json", clients = [], status = None):
 		self.__log = log
 		self.__users = users
+		self.clients = clients
 		self.port =  int(input("Порт:"))
-	@property
+		self.sock = None
+		self.status = status
+		self.startServer()
+
 	def startServer(self):
-		self.port = self.check_port()
-		print(f"Ваш порт {self.port}")
+		self.sock = socket.socket()
+		#self.port = self.check_port()
+		self.sock.bind(('', self.port))
+		self.sock.listen(5)
 		while True:
-			sock = socket.socket()
-			sock.bind(('', self.port))
-			sock.listen(1)
-			conn, addr = sock.accept()
-			print(self.checkUser(addr, conn))
+			conn, addr = self.sock.accept()
 			self.serverStarted(addr)
-			data = list()
-			while True:
-				msg = conn.recv(1024)
-				if msg:
-					data.append(msg.decode())
-					print(data[-1])
-				else:
-					conn.close()
-					self.serverStopped(addr)
-					break
+			Thread(target = self.listenToClient,args = (conn,addr)).start()
+			self.clients.append(conn)
+			#self.checkUser(addr,conn)
+			# Thread(target=self.recv).start()
+	# def check_port(self):
+	# 	self.port = self.port if (self.port >= 0 and self.port <= 65535)  else  9090
+	# 	for port in range(self.port, 65536):
+	# 		try:
+	# 			sock = socket.socket()
+	# 			sock.bind(('',port))
+	# 			sock.close
+	# 			print(f"Ваш порт {self.port}")
+	# 			return port
+	# 		except:
+	# 			pass
+	# 	raise("Something went wrong")
+		
+			#Thread(target=self.recv).start()
+			# print(self.checkUser(addr, conn))
+			# self.serverStarted(addr)
+	def broadcast(self,msg): 
+		for sock in self.clients:
+			data = pickle.dumps(["message",msg])
+			sock.send(data)
+	'''
+	Проверка порта, который мы будем использовать на нашем
+	сервере!
 
-	def check_port(self):
-		self.port = self.port if (self.port >= 0 and self.port <= 65535)  else  9090
-		for port in range(self.port, 65536):
-			try:
-				sock = socket.socket()
-				sock.bind(('',port))
-				sock.close
-				return port
-			except:
-				pass
-		assert False, "Порт не найден"
-	
-	def checkUser(self, addr, conn):
-		try:
-			open(self.__users).close()
-		except FileNotFoundError:
-			open(self.__users, 'a').close()
-		with open(self.__users, "r") as f:
-			try:
-				users = json.load(f)
-				name = users[str(addr[0])]['name']
-				conn.send("Введите свой пароль: ".encode('utf-8'))
-				passwd = conn.recv(1024).decode()
-				print("Currect!" if self.checkPasswrd(passwd,users[str(addr[0])]['password']) else "Incorrect")
-			except:
-				conn.send("Привет. Я тебя не знаю. Скажи мне свое имя: ".encode())
-				name = conn.recv(1024).decode()
-				conn.send("Введите свой пароль:".encode())
-				passwd = self.generateHash(conn.recv(1024).decode())
-				with open(self.__users, "w", encoding="utf-8") as f:
-					json.dump({addr[0] : {'name': name, 'password': passwd} },f)
-		return f"Здравствуйте, {name}"
-
+	'''	
 	def checkPasswrd(self, passwd, userkey) -> bool:
 		key = hashlib.md5(passwd.encode() + b'salt').hexdigest()
 		return key == userkey
@@ -71,6 +65,19 @@ class Server():
 	def generateHash(self, passwd) -> bytes:
 		key = hashlib.md5(passwd.encode() + b'salt').hexdigest()
 		return key
+	
+	def listenToClient(self, conn, address):
+		self.checkUser(address,conn)
+		while True:
+				data = conn.recv(1024)
+				if data:
+					status , data = pickle.loads(data)
+					if status == "message":
+						print(data)
+						self.broadcast(data)
+				else:
+					conn.close()
+					break
 
 	def serverStarted(self,ip):
 		with open(self.__log, "a", encoding="utf-8") as f:
@@ -78,5 +85,50 @@ class Server():
 	def serverStopped(self,ip):
 		with open(self.__log, "a", encoding="utf-8") as f:
 			print(f"{datetime.now().time()} Server Stopped {ip}", file = f)
+
+	def checkUser(self, addr, conn):
+		try:
+			open(self.__users).close()
+		except FileNotFoundError:
+			open(self.__users, 'a').close()
+		with open(self.__users, "r") as f:
+			try:
+				print("Here")
+				users = json.load(f)
+				name = users[str(addr[0])]['name']
+				conn.send(pickle.dumps(["passwd","Введите свой пароль: "]))
+				passwd = conn.recv(1024).decode()
+				conn.send(pickle.dumps(["success",f"Здравствуйте, {name}"])) if self.checkPasswrd(passwd,users[str(addr[0])]['password']) else print("Incorrect")
+			except:
+				conn.send("Привет. Я тебя не знаю. Скажи мне свое имя: ".encode())
+				name = conn.recv(1024).decode()
+				conn.send("Введите свой пароль:".encode())
+				passwd = self.generateHash(conn.recv(1024).decode())
+				with open(self.__users, "w", encoding="utf-8") as f:
+					json.dump({addr[0] : {'name': name, 'password': passwd} },f)
+
+
+	'''
+	успешное подключение
+	'''
+	# def success(self,userName, conn):
+	# 	self.clients.append((userName, conn))
+
+
+	'''
+	отправка сообщений от пользователя к пользователю
+	'''
+	# def sendMessage(self,fromUser,toUser, message):
+	# 	conn = None
+	# 	for user in self.clients:
+	# 		if user[0] == toUser:
+	# 			conn = user[1]
+	# 			break
+	# 	if conn:
+	# 		data = pickle.dumps(["message", fromUser, message])
+	# 		self.sock.send(data)
+
+	'''
+	Принятие сообщений
+	'''
 server = Server()
-server.startServer
