d2a37c163d7567cd933ebb334d29605dbc3983ca
Kirill <kirilllee1998@gmail.com>
2019-10-07 22:43:54 +0300

    kekuskus

--- client.py
+++ client.py
@@ -1,35 +1,35 @@
- import socket
+import socket
 
- sock = socket.socket()
- sock.setblocking(1)
- sock.connect(('10.38.165.12', 9090))
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('10.38.165.12', 9090))
 
- #msg = input()
- msg = "Hi!"
- sock.send(msg.encode())
+msg = input()
+msg = "Hi!"
+sock.send(msg.encode())
 
- data = sock.recv(1024)
- print('Put hostname:')
-  hostname = input()
-  print('Put Port:')
-  port = input()
+data = sock.recv(1024)
+print('Put hostname:')
+hostname = input()
+print('Put Port:')
+port = input()
 
- sock.close()
- if int(port) > 1024 and int(port) < 65000:
-              port1 = int(user_port)
-          else:
-              port1 = 9094
+sock.close()
+if int(port) > 1024 and int(port) < 65000:
+            port1 = int(user_port)
+else:
+            port1 = 9094
 
- print(data.decode())
- sock.connect((hostname, port1))
- msg = "Hi!"
- while True:
-     mess = input()
-     print(f'Your message: {mess}')
-     sock.send(mess.encode())
-     data = sock.recv(1024)
-     print(data.decode())
-     if mess == 'exit':
-         print('exit')
-         sock.close()
-         break
+print(data.decode())
+sock.connect((hostname, port1))
+msg = "Hi!"
+while True:
+    mess = input()
+    print('Your message: {mess}')
+    sock.send(mess.encode())
+    data = sock.recv(1024)
+    print(data.decode())
+    if mess == 'exit':
+        print('exit')
+        sock.close()
+        break
--- server.py
+++ server.py
@@ -1,37 +1,37 @@
 import socket
 
- sock = socket.socket()
- sock.bind(('', 9090))
- sock.listen(0)
- conn, addr = sock.accept()
- print(addr)
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(0)
+conn, addr = sock.accept()
+print(addr)
 
- msg = ''
- logf = open("ser.log", "_")
+msg = ''
+logf = open("ser.log", "_")
 
- sock = socket.socket()
- sock.bind(("",9094))
- sock.listen(5)
- while True:
- 	data = conn.recv(1024)
- 	if not data:
- 		break
- 	msg += data.decode()
- 	conn.send(data)
+sock = socket.socket()
+sock.bind(("",9094))
+sock.listen(5)
+while True:
+       data = conn.recv(1024)
+       if not data:
+       	break
+       msg += data.decode()
+       conn.send(data)
 
- print(msg)
+print(msg)
 
- conn.close()
-     conn, addr = sock.accept()
-     print(addr)
-     msg =''
-     while True:
-     	data = conn.recv(1024)
-     	if not data:
-     		break
-     	logf.write(f'Data: {data.decode()}')
-     	msg = data.decode()
-     	logf.write((f'MSG: {msg}')
-     	conn.send(msg.encode())
-     conn.close()
-     logf.close()
+conn.close()
+conn, addr = sock.accept()
+print(addr)
+msg =''
+while True:
+       data = conn.recv(1024)
+       if not data:
+              break
+       logf.write('Data: {data.decode()}')
+       msg = data.decode()
+       logf.write(('MSG: {msg}')
+       conn.send(msg.encode())
+conn.close()
+logf.close()
