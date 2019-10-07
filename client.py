d7ec9728225cc08d8b43307ad59e53e90c2ba58b
krasnovaol <mr.february@bk.ru>
2019-10-07 23:06:55 +0300

    Initial 'exit' for client (ex.2)

--- client.py
+++ client.py
@@ -3,14 +3,16 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+print('Для выхода введите команду "exit".')
 
-data = sock.recv(1024)
+msg = ''
+
+while msg != 'exit':
+    print('Введите сообщение:')
+    msg = input()
+    sock.send(msg.encode())
+    data = sock.recv(1024)
 
 sock.close()
-
-print(data.decode())
--- server.py
+++ server.py
@@ -10,11 +10,11 @@ msg = ''
 
 while True:
 	data = conn.recv(1024)
-	if not data:
+    
+	if data.decode() == 'exit':
 		break
+    
 	msg += data.decode()
 	conn.send(data)
-
-print(msg)
 
 conn.close()
