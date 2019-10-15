107525b30dfe85f6772e144fc021889697e06583
Tiffany Black (Farbovskaya) <55450516+TiffanyB1ack@users.noreply.github.com>
2019-10-15 18:12:26 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,25 +1,26 @@
 import socket
 
-print("Открываем сервер")
+f=open('t.txt','w')
+f.write("Открываем сервер")
 sock = socket.socket()
   while True:
   sock.bind(("", 9093))
   print("Прослушиваем порт")
   sock.listen(1)
 
-  print("Подключаем клиента")
+  f.write("Подключаем клиента")
   conn, addr = sock.accept()
   print(addr)
 
-  print("Принемаем данные от клиента")
+  f.write("Принемаем данные от клиента")
   data=conn.recv(1024)
   print(data.decode())
   if (data.decode() == "Exit"0):
-    print("Отключаем клиента. Пока-пока")
+    f.write("Отключаем клиента. Пока-пока")
     conn.close
   
-  print("Отправляем данные клиенту");
+  f.write("Отправляем данные клиенту")
   conn.send(b"hi")
   conn.send(('\nIP {}'.format(addr[0])).encode())
-print("Стопаем сервак")
+f.write("Стопаем сервак")
 sock.close
