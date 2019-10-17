7bc6fbb1307c6be9a86f45ef1c62851dbc87777b
krasnovaol <mr.february@bk.ru>
2019-10-17 18:37:28 +0300

    to ex.10 (inclusive)

--- client.py
+++ client.py
@@ -1,36 +1,88 @@
 import socket
 import re
 
-sock = socket.socket()
-sock.setblocking(1)
 
-host = input('Введите имя хоста:')
+def msg_s(msg):
+    global sock
+    length_msg = str(len(msg))
+    length_msg = '0' * (10 - len(length_msg)) + length_msg
+    msg = length_msg + msg
+    sock.send(msg.encode())
+
+
+def msg_r():
+    global sock
+    try:
+        length_msg = int(sock.recv(10).decode())
+    except ValueError:
+        msg = None
+    else:
+        msg = sock.recv(length_msg).decode()
+    return msg
+
+
+sock = socket.socket()
+
+host = input('Введите имя хоста (enter для значения по умолчанию):')
 cond = False
 
-if host != 'localhost':
-    cond = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", host)
-else:
+if host == 'localhost' or host == '':
     host = '127.0.0.1'
-    
-while (cond == False):
+    cond = True
+else:
+    cond = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", host)
+
+while not cond:
     host = input('Имя хоста введено неверно. Введите имя хоста:')
     cond = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", host)
 
-port = int(input('Введите номер порта:'))
+while True:
+    port = input('Введите номер порта (enter для значения по умолчанию):')
+    if port == '': port = 9090
+    if 1023 > int(port) > 49152:
+        print('Неправильный порт')
+        continue
+    else:
+        port = int(port)
+        break
 
-if (1023 > port) and (port > 49152):
-    port = 9090
-    
 sock.connect((host, port))
+
+data = msg_r()
+
+if int(data):
+    print("Введите ваше имя: ")
+    name = input()
+    msg_s(name)
+    while True:
+        print("Введите пароль")
+        password = input()
+        print("Повторите пароль")
+        password2 = input()
+        if password == password2:
+            break
+    msg_s(password)
+else:
+    while True:
+        print('Введите пароль: ')
+        password = input()
+        msg_s(password)
+        flag = msg_r()
+        if flag == "0":
+            msg_s('1')
+            break
+
+data = msg_r()
+print(data)
 
 print('Для выхода введите команду "exit".')
 
 msg = input("Введите сообщение: ")
 
 while msg != 'exit':
-    sock.send(msg.encode())
-    data = sock.recv(1024)
-    print(data.decode())
-    msg = input('Введите сообщение:')
-    
+    msg_s(msg)
+    data = msg_r()
+    print(data)
+    msg = input("Введите сообщение: ")
+
 sock.close()
--- server.py
+++ server.py
@@ -1,41 +1,102 @@
 import socket
 
-sock = socket.socket()
-f=open('log_file_server', 'a')
 
-port = int(input('Введите номер порта:'))
-if (1023 < port < 49152):
-    f.write(f'Клиент ввел номер порта: {port} \n')
-else:
+def msg_s(msg, conn):
+    length_msg = str(len(msg))
+    length_msg = '0' * (10 - len(length_msg)) + length_msg
+    msg = length_msg + msg
+    conn.send(msg.encode())
+
+
+def msg_r(conn):
+    try:
+        length_msg = int(conn.recv(10).decode())
+    except ValueError:
+        msg = None
+    else:
+        msg = conn.recv(length_msg).decode()
+    return msg
+
+
+try:
+    file = 'log_file_server.txt'
+    sock = socket.socket()
+
     port = 9090
-    f.write(f'Клиент ввел неверный номера порта. Порту автоматически присвоен номер {port}')
 
-while True:
-	try:
-		sock.bind(('', port))
-		break
-	except:
-		if port > 49151:
-			port = 9090
-		else:
-			port += 1
-            
-sock.listen(1)
-print(f'Слушаю порт с номером {port}')
-f.write('\nСлушаю порт..\n')
-
-while True:
-    f=open('log_file_server', 'a')
-    conn, addr = sock.accept()
-    data = conn.recv(1024)
-    f.write(f'Подключился клиент с {addr}\n')
-    if not data:
-            f.write(f'{addr} отключился\n')
+    while True:
+        try:
+            sock.bind(('', port))
+        except:
+            port += 1
+        else:
+            with open(file, 'a') as f:
+                f.write('Запуск сервера\n')
             break
-   
-    conn.send(data)
-    f.write(f'Отправляю данные {addr}\n')
-    f.close()
 
-conn.close()
-f.close()
+    sock.listen(1)
+    with open(file, 'a') as f:
+        f.write(f'Начало прослушивания порта {port}\n')
+    print(f'Слушаю порт с номером {port}')
+
+    try:
+        with open('users.txt', 'r') as users:
+            users = eval(users.read())
+    except FileNotFoundError:
+        with open('users.txt', 'w') as users:
+            users = {}
+    except SyntaxError:
+        users = {}
+
+    while True:
+        conn, addr = sock.accept()
+        addr = addr[0]
+
+        try:
+            if addr not in users.keys():
+                flag = '1'
+                msg_s(flag, conn)
+                name = msg_r(conn)
+                password = msg_r(conn)
+                users[addr] = users.get(addr, [name, password])
+                with open('users.txt', 'w') as u:
+                    print(users, file=u)
+            else:
+                flag = '0'
+                msg_s(flag, conn)
+                passwd = users[addr][1]
+                password = msg_r(conn)
+                while True:
+                    if passwd == password:
+                        msg_s('0', conn)
+                        msg_r(conn)
+                        break
+                    else:
+                        msg_s('1', conn)
+                        password = msg_r(conn)
+            msg = 'Hello ' + users[addr][0]
+            msg_s(msg, conn)
+
+            with open(file, 'a') as f:
+                f.write(f'Подключился клиент с {addr}\n')
+
+            while True:
+                data = msg_r(conn)
+                with open(file, 'a') as f:
+                    f.write(f'Получаю данные от {addr}\n')
+                if not data:
+                    with open(file, 'a') as f:
+                        f.write(f'{addr} отключился\n')
+                    conn.close()
+                    break
+
+                msg_s(data, conn)
+                with open(file, 'a') as f:
+                    f.write(f'Отправляю данные {addr}\n')
+        except ConnectionResetError:
+            continue
+
+except KeyboardInterrupt:
+    with open(file, 'a') as f:
+        f.write('Закрытие сервера')
+    sock.close()
