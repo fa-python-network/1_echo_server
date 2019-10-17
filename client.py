f380f6f45c43d3db6a12b397d86980702aeef867
grenadinio <56429768+grenadinio@users.noreply.github.com>
2019-10-17 21:55:01 +0300

    Maksimov PI18-2

--- client.py
+++ client.py
@@ -3,14 +3,34 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('127.0.0.1', 9090))
 
-#msg = input()
-msg = "Hi!"
+msg = input()
+msg = "Hello!"
 sock.send(msg.encode())
 
 data = sock.recv(1024)
+print('Put the hostname:')
+hostname = input()
+print('Put the port:')
+port = input()
 
 sock.close()
+if int(port) > 1024 and int(port) < 65000:
+    port_one = int(user_port)
+else:
+    port_one = 9099
 
 print(data.decode())
+sock.connect((hostname, port_one))
+msg = "Hello!"
+while True:
+    mes = input()
+    print('Your mesage: {mes}')
+    sock.send(mes.encode())
+    data = sock.recv(1024)
+    print(data.decode())
+    if mes == 'exit':
+        print('exit')
+        sock.close()
+        break
--- server.py
+++ server.py
@@ -3,18 +3,36 @@ import socket
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(0)
-conn, addr = sock.accept()
+con, addr = sock.accept()
 print(addr)
 
 msg = ''
+logf = open("ser.log", "_")
+
+sock = socket.socket()
+sock.bind(("", 9094))
+sock.listen(5)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    data = con.recv(1024)
+    if not data:
+        break
+    msg += data.decode()
+    con.send(data)
 
 print(msg)
 
-conn.close()
+con.close()
+con, addr = sock.accept()
+print(addr)
+msg = ''
+while True:
+    data = con.recv(1024)
+    if not data:
+        break
+    logf.write('Data: {data.decode()}')
+    msg = data.decode()
+    logf.write(('Message: {msg}'))
+    con.send(msg.encode())
+con.close()
+logf.close()
