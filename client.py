4a02f2c66fb183d551927b2485618a0f06c4e185
kozhevnikovnv <54978705+kozhevnikovnv@users.noreply.github.com>
2019-10-20 18:31:49 +0300

    Кожевников ПИ18-2 Эхо
    Кожевников ПИ18-2 Эхо

--- client.py
+++ client.py
@@ -2,15 +2,36 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+host = input('Введите имя хоста: ')
+if host == 'localhost':
+    pass
+else:
+    if any(c.isalpha() for c in host) == True:
+        print('Введено некорректное имя хоста.По умолчанию выбран локальный хост')
+        host = 'localhost'
+    else:
+        host_lst = host.split('.')           
+        for i in host_lst:
+            if 0 <= int(i) <= 255:
+                pass
+            else:
+                host = 'localhost'
+                print('Введено некорректное имя хоста.По умолчанию выбран локальный хост')
 
-data = sock.recv(1024)
+try:
+    port = int(input('Введите номер порта: '))
+    if 0 <= port <= 65535:
+        pass
+    else:
+        print('Введен некорректный номер порта.Номер порта по умолчанию 9090')
+        port = 9090
+    
+except ValueError:
+    print("Некорректный номер порта. Изначально: 9090")
+    port = 9090
+    
+sock.connect((host, port))
 
-sock.close()
-
-print(data.decode())
+print('Напишите exit для завершения работы с сервером')
+msg = ''
