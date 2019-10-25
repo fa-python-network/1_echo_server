a9c429029744707558487bb8629aa3a2173a6bf0
Сергей Ефремов <yefr@MacBook-Pro-Sergej-3.local>
2019-10-26 00:02:27 +0300

    Zadanie 7

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 import socket
+import json
+
+def write_into_json(dct):
+	with open('users.json', 'w') as f:
+		json.dump(dct, f)
+users = {}
 
 port = 9089
 file = open("logfile.txt", "a")
 sock = socket.socket()
+
+with open('users.json','r') as f:
+	users = json.load(f)
+
 try:
 	sock.bind(('', port))
 except OSError: 
@@ -12,12 +22,23 @@ except OSError:
 	file.write(f'Сервер использует порт{port}\n')
 sock.listen(1)
 file.write(f'Сервер слушает клиента{port}\n')
+
 while True:
+	conn, addr = sock.accept()
+	print(addr[0])
 	try:
-		conn, addr = sock.accept()
-	except sock.error:
-		pass
+		c = users[addr[0]]
+	except KeyError:
+ 		conn.send(b"What is your name?")
+ 		users[addr[0]] = (conn.recv(1024)).decode()
+ 		print(users[addr[0]])
+ 	msg_to_client = 'Hi,'+{users[addr[0]]}+'!'
+ 	msg_to_client = f'Hi,{users[addr[0]]}!'
+ 	conn.send(msg_to_client.encode())
+ 	write_into_json(users)
+
 	msg = ''
+
 	while True:
 		file.write(f'Сервер получает данные{addr}\n')
 		data = conn.recv(1024)
