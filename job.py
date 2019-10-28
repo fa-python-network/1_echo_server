5199c6f68f71aef6d2650a2cd8fb896bcdba7f9e
Tiffany Black (Farbovskaya) <55450516+TiffanyB1ack@users.noreply.github.com>
2019-10-28 14:12:53 +0300

    Update job.py

--- job.py
+++ job.py
@@ -22,34 +22,35 @@ sock = socket.socket()
       
     
   f.write("Подключаем клиента")
-  conn, addr = sock.accept()
-  for lines in names: #моя странная реализация кукки
-    if lines[1]==conn:
-      print('Hello', lines[2])
-      password=str(input(lines[2]," Enter your password:")
-      While (password<>lines[3]):
-         password=str(input(lines[2]," Your password is wrong. Enter it again:")
+  while true: #многопользовательский чат
+    conn, addr = sock.accept()
+    for lines in names: #моя странная реализация кукки
+      if lines[1]==conn:
+        print('Hello', lines[2])
+        password=str(input(lines[2]," Enter your password:")
+        While (password<>lines[3]):
+          password=str(input(lines[2]," Your password is wrong. Enter it again:")
       i=1
-  if i=0:
-    name.write(input("Enter your name:")
-    password=str(input("Enter your password:")
-    password2=str(input("Enter your password again:")
-    while (password1<>password2):
-       password2=str(input("Error. Passwords in not shodatsya! Enter your password again:")
-    names.write(password)
+   if i=0:
+      name.write(input("Enter your name:")
+     password=str(input("Enter your password:")
+     password2=str(input("Enter your password again:")
+      while (password1<>password2):
+        password2=str(input("Error. Passwords in not shodatsya! Enter your password again:")
+      names.write(password)
   
 
-  f.write("Принемаем данные от клиента")
-  data=conn.recv(1024)
-  print(data.decode())
-  if (data.decode() == "Exit"0):
-    f.write("Отключаем клиента. Пока-пока")
-    conn.close
- 
-  conn.sendstxt #выполняем функцию принятия сообщений от клиента
-                     
-  f.write("Отправляем данные клиенту")
-  conn.send(b"hi")
-  conn.send(('\nIP {}'.format(addr[0])).encode())
-f.write("Стопаем сервак")
+    f.write("Принемаем данные от клиента")
+    data=conn.recv(1024)
+    print(data.decode())
+    if (data.decode() == "Exit"0):
+      f.write("Отключаем клиента. Пока-пока")
+      conn.close
+
+    conn.sendstxt #выполняем функцию принятия сообщений от клиента
+
+    f.write("Отправляем данные клиенту")
+    conn.send(b"hi")
+    conn.send(('\nIP {}'.format(addr[0])).encode())
+  f.write("Стопаем сервак")
 sock.close
