337810f6a0533674daf4159e43e3e186e6a80235
nect88 <55210686+nect88@users.noreply.github.com>
2019-10-28 14:50:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
+print("Запуск сервера")
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
+sock.bind(('', 9089))
+print("Начало прослушивание порта")
+sock.listen(1)
 conn, addr = sock.accept()
 print(addr)
 
 msg = ''
 
+print("Прием данных от клиент")
 while True:
 	data = conn.recv(1024)
 	if not data:
 		break
 	msg += data.decode()
+	print("Отправка данных клиенту")
 	conn.send(data)
 
+print("Отключение клиента")
 print(msg)
 
+print("отключение сервера")
 conn.close()
