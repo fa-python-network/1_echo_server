8d20f1a59833ce76d4ea7014f761f4c7af1f6d3d
daria143 <55450503+daria143@users.noreply.github.com>
2019-10-28 18:17:06 +0300

    Полынцова ПИ18-4

--- client.py
+++ client.py
@@ -1,16 +1,49 @@
 import socket
+import re
+
+while True:
+    port = input('Введите порт от 1024 до 65535: \n')
+    if not port.isnumeric():
+        print('Ошибка')
+    elif 1023 <= int(port) <= 65535:
+        break
+    else:
+        print('Ошибка: порт не входит в нужный диапазон')
+while True:
+    ip = input('Введите ip сервера или оставьте пусстым для значения localhost: \n')
+    if ip == '':
+        ip = 'localhost'
+        break
+    elif re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip) == None:
+        print('Ошибка')
+    else:
+        break
 
 sock = socket.socket()
+sock.setblocking(1)
+sock.connect((ip, int(port)))
+# Аутентификация
+print(sock.recv(1024).decode())
+aut = sock.recv(1024).decode()
+print(aut)
+if (aut == 'Input your name:'):
+    sock.send(input().encode())
+    print(sock.recv(1024).decode())
+    sock.send(input().encode())
+else:
+    while True:
+        sock.send(input().encode())
+        answer = sock.recv(1024).decode()
+        if answer[0] == 'C':
+            print(answer)
+            break
+        print(answer)
+        print(sock.recv(1024).decode())
 
-sock.connect(('localhost', 9095))
-
-msg = "Hello, daria"
-sock.send(msg.encode())
-
-response = sock.recv(1024).decode()
-print(response)
-
-s = str(input('Tell someone:'))
-if s = 'exit':
-  sock.close()
- 
+while True:
+    msg = input("Input message: ")
+    if msg == "exit":
+        sock.close()
+        break
+    sock.send(msg.encode())
+    data = sock.recv(1024)
--- server.py
+++ server.py
@@ -1,20 +1,70 @@
 import socket
+import json
+
+f = open ('log.txt', 'w')
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+port = 9090
+while True:
+    try:
+        if port == 6553 :
+            print('Все порты заняты')
+            break
+        sock.bind(('', port))
+        break
+    except:
+        port += 1
 
-msg = ''
+print(f'Port number: {port}')
+print("Server starts", file = f)
+sock.listen(0)
+print("Now listen", file = f)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
+    conn, addr = sock.accept()
+    with open('base.json', 'r+') as d:
+		data = json.loads(d.read())
+		for i in data['clients']:
+			if i['ip'] == addr[0]:
+				hello = f"Hello {i['name']}"
+				conn.send(hello.encode())
+				while True:
+					conn.send(b'Input your password:')
+					passwd = conn.recv(1024).decode()
+					if i['password'] == passwd:
+						conn.send(b'Correct password!')
+						break
+					else:
+						conn.send(b'Wrong password! Try another')
+				break
+		else:
+			conn.send(b'Input your name:')
+			name = conn.recv(1024).decode()
+			conn.send(b'Input your password:')
+			passwd = conn.recv(1024).decode()
+			newClient = {"ip": addr[0], "name": name, "password": passwd}
+			data['clients'].append(newClient)
+			d.seek(0)
+			d.write(json.dumps(data))
+
+
+    msg = ''
+    while True:
+		data = conn.recv(1024)
+		print("new data from client", file = f)
+		if not data:
+			break
+		msg = msg + data.decode() + ' '
+		conn.send(data)
+
+    print("data to client", file = f)
+    print(f'Сообщение от пользователя: "{msg}"')
+    conn.close()
+    print('stop client', file = f)
+    # Остановка сервера
+    i = input('Если вы хотите остановить сервер введите: "stop"\n ')
+    if i == 'stop':
+		sock.close()
+		print('stop client', file = f)
+		f.close()
 		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
