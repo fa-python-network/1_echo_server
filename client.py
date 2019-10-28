70781e020b28f678d1ee855648a86fd8920a1c3a
nect88 <55210686+nect88@users.noreply.github.com>
2019-10-28 14:55:29 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,21 @@
 import socket
+print("Запуск")
 from time import sleep
 
+print("Установка соединения")
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('10.38.165.12', 9089))
 
-#msg = input()
-msg = "Hi!"
+print("Получения данных с консоли и отправка данных")
+msg = input()
 sock.send(msg.encode())
 
+print("Прием данных от сервера")
 data = sock.recv(1024)
 
+print("закрытие")
 sock.close()
-
+print("Вывод данных")
 print(data.decode())
+print("Завершение программы")
