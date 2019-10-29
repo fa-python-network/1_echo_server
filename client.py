cbc13ace6502ab276a0292eead3628f50ac55592
nect88 <55210686+nect88@users.noreply.github.com>
2019-10-29 19:38:00 +0300

    Update client.py

--- client.py
+++ client.py
@@ -13,15 +13,14 @@ sock.setblocking(1)
 sock.connect((sIP, Sport))
 
 msg = " "
-While (msg == "exit"): 
+While (msg != "exit"): 
   print("Получения данных с консоли и отправка данных")
   msg = input()
   sock.send(msg.encode())
   print("Прием данных от сервера")
   data = sock.recv(1024)
+  print(data.decode())
 
 print("закрытие")
 sock.close()
-print("Вывод данных и закрытие соединения")
-print(data.decode())
 print("Завершение программы")
