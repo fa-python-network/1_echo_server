bf17e877aa32bd548b4d4443c840b172e489be3f
Besong-dev <56872025+Besong-dev@users.noreply.github.com>
2019-10-29 16:04:09 +0300

    Add files via upload
    С наилучшими пожеланиями, Бесонг Тидиан Данни Блек

---
+++ client_1.py
@@ -0,0 +1,22 @@
+import socket
+import random
+sock = socket.socket()
+sock.connect(('localhost', 8080))
+while True:
+	print('Ожидайте ответа: ')
+	msg = sock.recv(1024).decode()
+	if msg != 'exit':
+		print('Server: ' + msg)
+	else:
+		print('Сервер отключился ')
+		break
+	
+	msg = input('Client: ')
+	if msg != 'exit':
+		sock.send(msg.encode('utf-8'))
+	else:
+		sock.send(msg.encode('utf-8'))
+		break
+sock.close()
+
+
