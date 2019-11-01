da40290f26f235ec2a7618f1b8565948cf47bb35
nastyapl77 <55870686+nastyapl77@users.noreply.github.com>
2019-11-01 13:25:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,43 @@
-import socket
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 09:15:46 2019
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+@author: 184136
+"""
 
-msg = ''
+from socket import socket
+
+
+sock = socket()
+print('Server started here')
+
+host, port = '', 8000
+
+sock.bind((host, port))
+sock.listen(5)
+print('Port listening started')
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    conn, addr = sock.accept()
+    addr = addr[0]
+    print(f'New client connected')
 
-print(msg)
+    data = b''
+    while True:
+        chunk = conn.recv(1024)
+        data += chunk
+        if len(chunk) < 1024:
+            break
 
-conn.close()
+    text = data.decode()
+    print(f'Sever have got "{text}" from {addr}')
+
+    conn.send(data)
+    print(f'{data} sent back to client')
+
+    conn.close()
+    print(f'Connection with {addr} finished')
+
+sock.close()
+
+print('Server stopped')
