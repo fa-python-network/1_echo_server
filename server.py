b684061a62945147a502726ce2139eee1f74731a
184140 <56712629+184140@users.noreply.github.com>
2019-11-01 13:49:21 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,35 @@
-import socket
+from socket import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+def main():
+    while True:
+        conn, addr = sock.accept()
+        addr = addr[0]
+        print(f'Connected to "{addr}"')
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+        data = ''
+        while True:
+            chunk = conn.recv(1024).decode()
+            data += chunk
+            if len(chunk) < 1024:
+                break
 
-print(msg)
+        print(f'Sever received "{data}" from {addr}')
 
-conn.close()
+        conn.send(data.encode())
+        print(f'{data.encode()} sent back to client')
+
+        conn.close()
+        print(f'{addr} disconnected')
+    
+
+with socket() as sock:
+    print('Server started here')
+
+    sock.bind(('', 8888))
+    sock.listen(8)
+    print('Port listening started')
+
+    main()
+
+print('"main" finished')
