7f38464b6ef4c1fb42b5269772bc25a9eb2e9748
Уральсков Евгений ПИ18-2 <45493202+UralskovEA@users.noreply.github.com>
2019-11-12 03:35:29 +0300

    Корявый групповой чат

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 import socket
+from time import sleep
 
+msg = ''
+while msg == '':
+	msg = input('Введите никнейм: ')
 sock = socket.socket()
 sock.connect(('localhost', 9090))
-print('Соединение с сервером')
-
-msg = '1'
+sock.setblocking(0)
+sock.send(msg.encode("utf-8"))
 
 while msg != 'exit':
-	print('Введите сообщение:')
 	msg = input()
 	sock.send(msg.encode("utf-8"))
-	data = sock.recv(1024)
-	print("Ответ: ", data.decode("utf-8"),"\n")
+	try:
+		data = sock.recv(1024)
+	except:
+		pass
+	else:
+		print(data.decode("utf-8"))
+	if msg != '':
+		print('Вы: ', msg)
+
 sock.close()
--- server.py
+++ server.py
@@ -1,27 +1,49 @@
 import socket
 from time import sleep
 
-sock = socket.socket()
-sock.bind(('', 9090))
-print("Сервер запущен")
-sock.listen(1)
-print("Начало прослушивания порта")
-conn, addr = sock.accept()
-print("Клиент ", addr," подключился.")
+port = 9090
+connections = {}
 
-msg = ''
+sock = socket.socket()
+sock.bind(('', port))
+print("Сервер запущен")
+sock.listen(10)
+sock.setblocking(0)
+print("Начало прослушивания порта")
 
 while True:
-	data = conn.recv(1024)
-	print("Прием данных")
-	if not data:
-		print("Клиент разорвал соединение")
-		break
-	msg += data.decode("utf-8")
-	conn.send(data)
-	print("Отправка данных")
+	try: 
+		conn, addr = sock.accept()
+	except:
+		pass
+	else:
+		connections[conn] = conn.recv(1024).decode()
+		for i in connections:
+			if i != conn:
+				i.send(('Новый участник: '+connections[conn]+'\n').encode())
+
+	for conn in connections:
+		conn.setblocking(0)
+		try: 
+			data = conn.recv(1024).decode()
+		except:
+			pass
+		else:
+			if data == 'exit':
+				data = (connections.pop(conn)+' покинул нас\n')
+				print(data)
+				sleep(2)
+				for i in connections:
+					if i != conn:
+						i.send(data.encode())
+				break
+			else:
+				for i in connections:
+					if i != conn:
+						i.send((connections[conn]+': '+data+'\n').encode())
+	sleep(1)
 
 print('Остановка сервера')
 sleep(10)
-
 conn.close()
+
