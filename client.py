986ab78d588969ff7aed4641b3defa72e7114ce0
Anna <avgovorova@mail.ru>
2020-05-20 14:41:29 +0000

    2nd exercise

--- client.py
+++ client.py
@@ -3,11 +3,13 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+sock.connect(('localhost', 9090))
+while True:
+    data = ""
+    msg = input("Enter your message to server or 'exit' to quit: ")
+    if msg.lower() == "exit":
+        break
+    sock.send(msg.encode())
 
 data = sock.recv(1024)
 
