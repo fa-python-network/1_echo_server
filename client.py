e795ae0e83fe309a9efa52577ab1beb4b3c64a5f
Denistopalu <denistopalu@mail.ru>
2020-05-21 00:31:00 +0300

    Работа Топалу Дениса

--- client.py
+++ client.py
@@ -1,16 +1,91 @@
+# coding=utf-8
 import socket
-from time import sleep
+import logging
+
+
+logging.basicConfig(filename="client.log", format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.info('Клиент запущен')
+
+def con(ip, sock, Name):
+    sock.setblocking(1)
+    sock.connect(ip)
+    sock.send(Name.encode())
+
+
+def recon(ip, sock):
+    sock.setblocking(1)
+    sock.connect(ip)
+
+
+def msg_send(sock, l, msg, key):
+    recon(l, sock)
+    crypt = ''
+    for i in msg:  #
+        crypt += chr(ord(i) ^ key)
+    msg = crypt
+    sock.send(msg.encode())
+
+
+
+def ip():
+    while True:
+        msg = input('IP сервера (пустая строка = localhost): ')
+        if msg == "":  # default
+            ip = '127.0.0.1'
+            print('IP сервера ' + ip)
+        else:
+            ip = msg
+            print('IP сервера ' + ip)
+        break
+    logging.info('Установлен ip адресс сервера '+ip)
+    return ip
+
+
+def port():
+    while True:
+        msg = input('Порт (пустая строка = 9090): ')
+        if msg == "":  # default
+            port = 9090
+            print('Порт 9090')
+        else:
+            port = int(msg)
+            print ('Порт ' + str(port))
+        break
+    return port
+
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+adr = ip()
+P = port()
+logging.info('Установлен порт '+str(P))
+l = (adr, P)
 
-msg = input()
-#msg = "Hi!"
-sock.send(msg.encode())
+while True:
+    try:
+        k = int(input('Введите ключ шифрования '))
+        logging.info('Введен ключ шифрования')
+        break
+    except:
+        logging.error("Введен неверный ключ")
+        print('Введите любое число, главное чтобы оно совпадало на сервере и клиенте')
 
-data = sock.recv(1024)
 
-sock.close()
+Name = 'name(' + str(input('Введите свое имя: ')) + ')'
+msg_send(sock, l, Name, k)
 
-print(data.decode())
+
+print ('\n Для выхода из чата напишите exit \n')
+
+while True:
+    sock = socket.socket()
+
+    msg = input('Введите сообщение: ')
+    if msg != 'exit':
+        logging.info('Отправленно сообщение ' + msg)
+        msg_send(sock, l, msg, k)
+    else:
+        print ('Всего хорошего!')
+        logging.info('Клиент завершил работу')
+        break
+    sock.close()
+logging.info('Клиент остановлен')
--- server.py
+++ server.py
@@ -1,20 +1,94 @@
+# coding=utf-8
 import socket
+import logging
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+logging.basicConfig(filename="server.log", format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.info('Сервер запущен')
 
-msg = ''
+def port(sock):
+    global port
+    while True:
+        try:
+            msg = input('Порт (пустая строка = 9090)')
+            if msg == "":  # default
+                port = 9090
+                sock.bind(('', port))
+                break
+            else:
+                port = int(msg)
+                sock.bind(('', port))
+                break
+        except:
+            logging.error("Порт "+int(port)+" занят")
+            print('Порт занят')
+            por = port +1
+            port = int(por)
+            break
+    return port
+
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    try:
+        key = int(input('Введите ключ шифрования '))
+        logging.info('Введен ключ шифрования')
+        break
+    except:
+        print('Введите любое число, главное чтобы оно совпадало на сервере и клиенте')
+        logging.error("Неверные символы ключа шифрования")
 
-print(msg)
+def message(conn, key):
+    msg = conn.recv(1024).decode()
+    crypt = ''
+    for i in msg:
+        crypt += chr(ord(i) ^ key)
+    return crypt
 
-conn.close()
+
+def login(addr, text,c,e):
+    global name
+    f = open('login_info.txt', 'a')
+    fr = open('login_info.txt', 'r')
+    a = sum(1 for line in open('login_info.txt', 'r'))
+    b = 0
+    if text[:4] == 'name':
+        for line in fr.readlines():
+            b += 1
+            li = line.partition(':')[2]
+            if li[:-1] == addr[0]:
+                name = line.partition(':')[0]
+                N = str(name)[1:-1]
+                if c==0:
+                    print ('Приветствуем вас, ' + N)
+                    logging.info('Залогинился пользователь '+name+addr[0])
+                    c=1
+                    break
+            if b == a and li[:-1] != addr[0]:
+                name = text[4:]
+                N = str(name)[1:-1]
+                f.write(name + ':' + str(addr[0]) + '\n')
+                if e==0:
+                    print ('Привет,' + N + '! Вы тут первый раз? Теперь вы зарегестрированны')
+                    logging.info('Зарегистрирован новый пользователь ' + name + addr[0])
+                    e=1
+                    break
+
+    f.close()
+    fr.close()
+    return name
+
+
+sock = socket.socket()
+P = port(sock)
+print ('Успешное подключение к порту ' + str(P))
+logging.info('Успешное подключение к порту ' + str(P))
+c=0
+e=0
+while True:
+    sock.listen(1)
+    conn, addr = sock.accept()
+    text = message(conn, key)
+    name = login(addr, str(text),c,e)
+    logging.info('Получено сообщение '+name+text)
+    print(name, text)
+    conn.close()
+logging.info('Сервер остановлен')
