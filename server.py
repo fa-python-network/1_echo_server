e3fec2bd1f9a6156661dcf00091b0766197e2fdc
Lomovtseva Ekaterina <elomovtseva.2000@mail.ru>
2020-05-20 18:52:43 +0300

    echo server Ekaterina Lomovtseva

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 import socket
+import logging as l
+
+format = '%(levelname)s %(asctime)s - %(message)s'
+l.basicConfig(filename='logs.log', format=format, datefmt='%d.%m.%Y %H:%M:%S', level=l.INFO)
+
+l.info('Start logging')
 
 sock = socket.socket()
 sock.bind(('', 9090))
+l.info('порт сервера: 9090')
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+l.info('Сервер работает')
 
+while True:
+    conn, addr = sock.accept()
+    l.info(f'Пользователь {addr} подключился к серверу')
 msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data)
+    l.info('Сервер отправляет ответ')
 
 conn.close()
+l.info('Соединение отключено')
