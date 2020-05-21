03d46e065eebdebbdfc0b96da7d6a2f0e3319001
Глеб Шашин <65556037+shamoro@users.noreply.github.com>
2020-05-21 11:23:08 +0300

    5 задание 1 лаб
    Сделал ,чтобы служебные сообщения отправлялись в лог файл.

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 import socket
 
+sock = socket.socket()
+sock.bind(('', 9090))
+log_set = open('file.log', 'a')
+log_set.write("start working\n")
+While True:
 
-while True:
-	sock = socket.socket()
-    sock.bind(('', 9090))
-    sock.listen(0)
-    conn, addr = sock.accept()
-    print(addr)
+        sock.listen(1)
+        log_set = open('file.log', 'a')
+        log_set.write("server is waiting for connections...\n")
+        conn, addr = sock.accept()
+        log_set.write("User was conect\n")
 
- msg = ''
- 
-while True:
-	    data = conn.recv(1024)
-	    if not data:
-		    break
-	    msg += data.decode()
-	    conn.send(data)
+        msg = ""
 
-print(msg)
+        break
 
-        conn.close()
+msg += data.decode()
+conn.send(data)
+
+log_set.write("Recieved message from user\n")
+
+conn.close()
+
