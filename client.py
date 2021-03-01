959e626d431445d994b6630f54c15627cc281785
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-03-01 13:40:49 +0300

    Переписали клиента на ООП
    А то как-то неудобно

--- client.py
+++ client.py
@@ -4,29 +4,35 @@ from time import sleep
 SERVER_IP = "127.0.0.1"
 PORT_NUMBER = 9090
 
+class Client:
+    def __init__(self) -> None:
+        sock = socket.socket()
+        sock.setblocking(1)
+        sock.connect((SERVER_IP, PORT_NUMBER))
+        self.sock = sock
+        #Работа с данными, поступающими от пользователя
+        self.user_processing()
+        #Закрываем сокет
+        self.sock.close()
+        
+    def user_processing(self):
+
+        while True:
+            msg = input("-> ")
+            #Если сообщение exit
+            if msg == "exit": break
+            #Если ничего не ввели
+            if msg == "": msg = "None"
+        
+            #Отправляем сообщение
+            self.sock.send(msg.encode())
+            #Получаем ответ
+            data = self.sock.recv(1024)
+
+            print(f"Ответ от сервера: {data.decode()}")
+
 def main():
-    sock = socket.socket()
-    sock.setblocking(1)
-    sock.connect((SERVER_IP, PORT_NUMBER))
+    client = Client()
 
-
-    #2. Модифицируйте код клиента таким образом, чтобы он читал строки в цикле до тех пор, пока клиент не введет “exit”.
-    while True:
-
-        msg = input("-> ")
-        #Если сообщение exit
-        if msg == "exit": break
-        #Если ничего не ввели
-        if msg == "": msg = "None"
-    
-
-        #Отправляем сообщение
-        sock.send(msg.encode())
-        #Получаем ответ
-        data = sock.recv(1024)
-
-        print(f"Ответ от сервера: {data.decode()}")
-
-    sock.close()
 if __name__ == "__main__":
 	main()
