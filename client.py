c08628dc3415fe6985ece704a672a41575dcdf52
Sergey_VM <2sergmir2@gmail.com>
2022-10-12 14:37:25 +0300

    Laba is done

--- client.py
+++ client.py
@@ -3,14 +3,16 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+print("Соединение с сервером")
+sock.connect(('10.4.32.58', 9090))
 
 #msg = input()
 msg = "Hi!"
+print("Отправка данных серверу")
 sock.send(msg.encode())
-
+print("Прием данных от сервера")
 data = sock.recv(1024)
-
+print("Разрыв соединения с сервером")
 sock.close()
 
 print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 
+print("Запуск сервера")
 sock = socket.socket()
 sock.bind(('', 9090))
+print("Начало прослушивания")
 sock.listen(0)
+print("Подключение клиента")
 conn, addr = sock.accept()
 print(addr)
 
 msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    print("Прием данных от клиента")
+    data = conn.recv(1024)
+    if not data:
+        break
+    msg += data.decode()
+    print("Отправка данных клиенту")
+    conn.send(data)
 
 print(msg)
-
+print("Отключение клиента")
+print("Отключение сервера")
 conn.close()
